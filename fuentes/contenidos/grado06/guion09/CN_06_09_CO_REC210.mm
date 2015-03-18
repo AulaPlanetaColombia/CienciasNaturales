d5bd04ff6a089a4b37eea24caba7cba02f901277
camipama <camipama@hotmail.com>
2015-03-18 08:57:05 -0500

    Manuscritos iniciales Grado 6 Guión 9
    Manuscritos iniciales sin estilo de grado 6 guión 9 - la materia y sus
    propiedades

---
+++ fuentes/contenidos/grado06/guion09/CN_06_09_CO_REC210.mm
@@ -0,0 +1,65 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425428791047" ID="Freemind_Link_556673953" MODIFIED="1425429825252" TEXT="La materia ">
+<node CREATED="1425429455355" ID="_" MODIFIED="1426685734436" POSITION="right" TEXT="Formada por:&#xe1;tomos ">
+<icon BUILTIN="idea"/>
+<node CREATED="1425429487528" ID="Freemind_Link_1881865852" MODIFIED="1425432219326" TEXT="Protones, neutrones y electrones "/>
+<node CREATED="1425429555569" ID="Freemind_Link_535843471" MODIFIED="1425429578006" TEXT="Agrupados en moleculas "/>
+</node>
+<node CREATED="1425429688599" ID="Freemind_Link_786692453" MODIFIED="1426685758085" POSITION="left" TEXT="sus estados f&#xed;sicos son">
+<icon BUILTIN="idea"/>
+<node CREATED="1425429854481" ID="Freemind_Link_666228875" MODIFIED="1425429886037" TEXT="S&#xf3;lido ">
+<node CREATED="1425430771717" ID="Freemind_Link_306773163" MODIFIED="1425431108179" TEXT="Se caracteriza por tener forma y volumen definido "/>
+</node>
+<node CREATED="1425429861993" ID="Freemind_Link_376192316" MODIFIED="1425429867247" TEXT="L&#xed;quido ">
+<node CREATED="1425430983877" ID="Freemind_Link_766392624" MODIFIED="1425431028128" TEXT="Se caracteriza por tener forma variable pero volumen constante "/>
+</node>
+<node CREATED="1425429869294" ID="Freemind_Link_173070074" MODIFIED="1425429872794" TEXT="Gas">
+<node CREATED="1425431039237" ID="Freemind_Link_425486784" MODIFIED="1425431077654" TEXT="Tiene forma y volumen variable. "/>
+</node>
+<node CREATED="1425429874460" ID="Freemind_Link_1953457738" MODIFIED="1425429879512" TEXT="Plasma">
+<node CREATED="1425431253809" ID="Freemind_Link_671101718" MODIFIED="1425432181243" TEXT="Particulas cargadas electricamente  en movimiento y reaccionando entre s&#xed; "/>
+</node>
+</node>
+<node CREATED="1425429898036" ID="Freemind_Link_1514936967" MODIFIED="1426685724960" POSITION="right" TEXT="Presenta unas propiedades ">
+<icon BUILTIN="idea"/>
+<node CREATED="1425431316779" ID="Freemind_Link_1989864240" MODIFIED="1425431326627" TEXT="Generales ">
+<node CREATED="1425431377494" ID="Freemind_Link_1094103829" MODIFIED="1425431558234" TEXT="No permiten diferenciar la materia  ">
+<node CREATED="1425431562304" ID="Freemind_Link_1563185999" MODIFIED="1425431566110" TEXT="Masa">
+<node CREATED="1426685791921" ID="ID_412405329" MODIFIED="1426685802531" TEXT="Cantidad de materia"/>
+</node>
+<node CREATED="1425431567713" ID="Freemind_Link_968811681" MODIFIED="1425431571188" TEXT="Volumen">
+<node CREATED="1426685804829" ID="ID_596814774" MODIFIED="1426685816490" TEXT="Espacio que ocupa la materia"/>
+</node>
+</node>
+</node>
+<node CREATED="1425431329592" ID="Freemind_Link_1757745889" MODIFIED="1425431341710" TEXT="Espec&#xed;ficos">
+<node CREATED="1425431579174" ID="Freemind_Link_1894501757" MODIFIED="1425431607368" TEXT="Permiten diferenciar la materia ">
+<node CREATED="1425431610398" ID="Freemind_Link_7702903" MODIFIED="1425431631177" TEXT="Densidad"/>
+<node CREATED="1425431634824" ID="Freemind_Link_865756897" MODIFIED="1425431646185" TEXT="Punto de fusi&#xf3;n"/>
+<node CREATED="1425431649298" ID="Freemind_Link_1120118270" MODIFIED="1425431657678" TEXT="Punto de ebullici&#xf2;n"/>
+</node>
+</node>
+</node>
+<node CREATED="1425431720974" ID="Freemind_Link_1963604224" MODIFIED="1426685737481" POSITION="right" TEXT="Constituye los materiales  ">
+<icon BUILTIN="idea"/>
+<node CREATED="1425431784997" ID="Freemind_Link_26772687" MODIFIED="1425431795498" TEXT="Que presentan propiedades ">
+<node CREATED="1425431799056" ID="Freemind_Link_1688336343" MODIFIED="1425431812721" TEXT="Cuantificables">
+<node CREATED="1425431847013" ID="Freemind_Link_1634271114" MODIFIED="1425431853619" TEXT="Elasticiadad"/>
+<node CREATED="1425431865837" ID="Freemind_Link_1106251293" MODIFIED="1425431873579" TEXT="Plasticidad"/>
+<node CREATED="1425431876463" ID="Freemind_Link_941930585" MODIFIED="1425431883021" TEXT="Fragilidad"/>
+<node CREATED="1425431885774" ID="Freemind_Link_1831101617" MODIFIED="1425431890817" TEXT="Resistencia"/>
+<node CREATED="1425431893464" ID="Freemind_Link_271306458" MODIFIED="1425431901604" TEXT="Dureza"/>
+<node CREATED="1425431903765" ID="Freemind_Link_998443025" MODIFIED="1425431910969" TEXT="Conductividad"/>
+<node CREATED="1425431913194" ID="Freemind_Link_1538170077" MODIFIED="1425431919403" TEXT="Impermeabilidad"/>
+<node CREATED="1425431922205" ID="Freemind_Link_748781899" MODIFIED="1425431926085" TEXT="Solubilidad"/>
+</node>
+<node CREATED="1425431815395" ID="Freemind_Link_619664826" MODIFIED="1425431823333" TEXT="No cuantificables">
+<node CREATED="1425431928175" ID="Freemind_Link_282610869" MODIFIED="1425431932038" TEXT="Color"/>
+<node CREATED="1425431933831" ID="Freemind_Link_1308171618" MODIFIED="1425431937656" TEXT="Olor"/>
+<node CREATED="1425431939899" ID="Freemind_Link_463250173" MODIFIED="1425431943211" TEXT="Sabor"/>
+</node>
+</node>
+</node>
+</node>
+</map>
