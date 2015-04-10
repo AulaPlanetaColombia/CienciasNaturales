2dbb6492504df2b4c010105e7d61fa11fd7a37f5
Maaljurem <miguelaljure@hotmail.com>
2015-04-10 11:42:31 -0500

    CN_04_08 con corrección de estilo
    Solicitudes gráficas no se envían hasta saber si los recursos están o no
    aprobados.

--- fuentes/contenidos/grado04/guion08/CN_04_08_REC210.mm
+++ fuentes/contenidos/grado04/guion08/CN_04_08_REC210.mm
@@ -1,75 +1,81 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1424706898027" ID="ID_1096688278" MODIFIED="1428427708929" TEXT="LA MATERIA" VGAP="4">
+<node CREATED="1424706898027" ID="ID_1096688278" MODIFIED="1427564555513" TEXT="La Materia" VGAP="4">
 <node CREATED="1425951474543" HGAP="61" ID="ID_1929767006" MODIFIED="1426001697590" POSITION="left" TEXT="tiene" VSHIFT="-32">
 <icon BUILTIN="idea"/>
-<node CREATED="1425951598017" ID="ID_397171002" MODIFIED="1428428067117" TEXT="Propiedades">
+<node CREATED="1425951598017" ID="ID_397171002" MODIFIED="1428683068907" TEXT="Propiedades">
 <node CREATED="1425951613404" ID="ID_231405379" MODIFIED="1426000045739" TEXT="como">
 <icon BUILTIN="idea"/>
-<node CREATED="1428428107667" ID="ID_378835455" MODIFIED="1428428274624" STYLE="bubble" TEXT="- Dureza&#xa;- Tenacidad&#xa;- Transparencia&#xa;- Impermeabilidad&#xa;- Conductividad t&#xe9;rmica&#xa;- Aislamiento t&#xe9;rmico&#xa;- Color&#xa;- Olor&#xa;- Sabor&#xa;"/>
+<node CREATED="1428422505165" ID="ID_1014269562" MODIFIED="1428683172792" STYLE="bubble" TEXT="- Dureza&#xa;- Tenacidad&#xa;- Transparencia&#xa;- Impermeabilidad&#xa;- Conductividad t&#xe9;rmica&#xa;- Conductividad el&#xe9;ctrica&#xa;- Color&#xa;- Olor&#xa;- Sabor"/>
 </node>
 </node>
 </node>
-<node CREATED="1424707681543" HGAP="31" ID="ID_805978295" MODIFIED="1428427724097" POSITION="right" TEXT="est&#xe1; formada por" VSHIFT="-32">
+<node CREATED="1424707681543" HGAP="31" ID="ID_805978295" MODIFIED="1426008535875" POSITION="right" TEXT="formada por" VSHIFT="-32">
 <icon BUILTIN="idea"/>
-<node CREATED="1424707707654" ID="ID_1491153972" MODIFIED="1428427734645" TEXT="&#xc1;tomos">
+<node CREATED="1424707707654" ID="ID_1491153972" MODIFIED="1428682753816" TEXT="&#xc1;tomos">
 <node CREATED="1424708413640" ID="ID_1895159105" MODIFIED="1426000666846" TEXT="agrupados en">
 <icon BUILTIN="idea"/>
-<node CREATED="1424708429700" ID="ID_1062899795" MODIFIED="1428427741372" TEXT="Mol&#xe9;culas"/>
+<node CREATED="1424708429700" ID="ID_1062899795" MODIFIED="1428682745629" TEXT="Mol&#xe9;culas"/>
 </node>
 </node>
 </node>
 <node CREATED="1425949634154" HGAP="24" ID="ID_930436624" MODIFIED="1426001881283" POSITION="left" TEXT="experimenta" VSHIFT="19">
 <icon BUILTIN="idea"/>
-<node CREATED="1425949915989" HGAP="31" ID="ID_894822336" MODIFIED="1428428193257" TEXT="Cambios qu&#xed;micos" VSHIFT="13">
+<node CREATED="1425949915989" HGAP="31" ID="ID_894822336" MODIFIED="1428683039856" TEXT="Cambios qu&#xed;micos" VSHIFT="13">
 <node CREATED="1425950017053" ID="ID_1197274583" MODIFIED="1426000769947" TEXT="por medio de">
 <icon BUILTIN="idea"/>
-<node CREATED="1425950290518" ID="ID_1384449694" MODIFIED="1428428199151" TEXT="Reacciones qu&#xed;micas">
+<node CREATED="1425950290518" ID="ID_1384449694" MODIFIED="1428683201132" TEXT="Reacciones qu&#xed;micas">
 <node CREATED="1425950302024" ID="ID_381888182" MODIFIED="1426000773113" TEXT="como">
 <icon BUILTIN="idea"/>
-<node CREATED="1428428203601" ID="ID_1359269591" MODIFIED="1428428274624" STYLE="bubble" TEXT="- Combusti&#xf3;n&#xa;- Oxidaci&#xf3;n&#xa;- Fermentaci&#xf3;n&#xa;"/>
+<node CREATED="1428422505165" ID="ID_61392839" MODIFIED="1428683251624" STYLE="bubble" TEXT="- Combusti&#xf3;n&#xa;- Oxidaci&#xf3;n&#xa;- Fermentaci&#xf3;n"/>
 </node>
 </node>
 </node>
 </node>
-<node CREATED="1425950237385" HGAP="32" ID="ID_804502573" MODIFIED="1428428229509" TEXT="Cambios f&#xed;sicos" VSHIFT="38">
+<node CREATED="1425950237385" HGAP="32" ID="ID_804502573" MODIFIED="1428683061652" TEXT="Cambios f&#xed;sicos" VSHIFT="38">
 <node CREATED="1425950637657" ID="ID_854781340" MODIFIED="1426001775085" TEXT="que son">
 <icon BUILTIN="idea"/>
-<node CREATED="1428428236658" ID="ID_976489802" MODIFIED="1428428274624" STYLE="bubble" TEXT="- Cambios de posici&#xf3;n&#xa;- Cambios de forma&#xa;- Cambios de tama&#xf1;o&#xa;- Cambios de estado&#xa;"/>
+<node CREATED="1428422505165" ID="ID_1964525875" MODIFIED="1428683315049" STYLE="bubble" TEXT="- Cambios de posici&#xf3;n&#xa;- Cambios de forma&#xa;- Cambios de tama&#xf1;o&#xa;- Cambios de estado"/>
 </node>
 </node>
 </node>
-<node CREATED="1426001634525" HGAP="14" ID="ID_1786500992" MODIFIED="1426002572494" POSITION="right" TEXT="se presenta en" VSHIFT="50">
+<node CREATED="1426001634525" HGAP="14" ID="ID_1786500992" MODIFIED="1427564557005" POSITION="right" TEXT="se presenta en" VSHIFT="50">
 <icon BUILTIN="idea"/>
-<node CREATED="1425950652141" HGAP="43" ID="ID_1440823686" MODIFIED="1428427801104" TEXT="Estados" VSHIFT="3">
+<node CREATED="1425950652141" HGAP="43" ID="ID_1440823686" MODIFIED="1428682851735" TEXT="Estados" VSHIFT="3">
 <node CREATED="1425950727278" ID="ID_977902271" MODIFIED="1426001718621" TEXT="como">
 <icon BUILTIN="idea"/>
-<node CREATED="1428427761359" ID="ID_260368965" MODIFIED="1428428052656" STYLE="bubble" TEXT="- S&#xf3;lido&#xa;- L&#xed;quido&#xa;- Gaseoso&#xa;"/>
+<node CREATED="1428422505165" ID="ID_1123024789" MODIFIED="1428682860936" STYLE="bubble" TEXT="- S&#xf3;lido&#xa;- L&#xed;quido&#xa;- Gaseoso"/>
 </node>
-<node CREATED="1425950799165" ID="ID_1548206402" MODIFIED="1428427820033" TEXT="que pueden alterarse">
+<node CREATED="1425950799165" ID="ID_1548206402" MODIFIED="1426003680155" TEXT="que pueden">
 <icon BUILTIN="idea"/>
-<node CREATED="1425950940717" ID="ID_1815859098" MODIFIED="1426003940010" TEXT="seg&#xfa;n">
+<node CREATED="1425950900213" ID="ID_1651219474" MODIFIED="1428682837379" TEXT="Alterarse">
+<node CREATED="1425950940717" ID="ID_1815859098" MODIFIED="1428682838161" TEXT="seg&#xfa;n">
 <icon BUILTIN="idea"/>
-<node CREATED="1425951079547" ID="ID_1754635729" MODIFIED="1428427892380" TEXT="Procesos de cambio de estado">
-<node CREATED="1425951091704" ID="ID_108440754" MODIFIED="1426003818343" TEXT="como">
+<node CREATED="1425951079547" ID="ID_1754635729" MODIFIED="1428682729647" TEXT="Procesos de cambio de estado">
+<node CREATED="1425951091704" ID="ID_108440754" MODIFIED="1428682736816" TEXT="como">
 <icon BUILTIN="idea"/>
-<node CREATED="1428427902133" ID="ID_1517311779" MODIFIED="1428428052677" STYLE="bubble" TEXT="- Fusi&#xf3;n&#xa;- Vaporizaci&#xf3;n&#xa;- Condensaci&#xf3;n&#xa;- Solidificaci&#xf3;n&#xa;- Sublimaci&#xf3;n&#xa;- Sublimaci&#xf3;n inversa&#xa;"/>
+<node CREATED="1428422505165" ID="ID_994673751" MODIFIED="1428682820341" STYLE="bubble" TEXT="- Fusi&#xf3;n&#xa;- Vaporizaci&#xf3;n&#xa;- Condensaci&#xf3;n&#xa;- Solidificaci&#xf3;n&#xa;- Sublimaci&#xf3;n&#xa;- Sublimaci&#xf3;n inversa"/>
 </node>
 </node>
 </node>
 <node CREATED="1426003718179" HGAP="26" ID="ID_267816578" MODIFIED="1426003965198" TEXT="debido a" VSHIFT="72">
 <icon BUILTIN="idea"/>
-<node CREATED="1426003739744" ID="ID_683947330" MODIFIED="1428427948327" TEXT="Cambios en la energ&#xed;a t&#xe9;rmica de las mol&#xe9;culas">
-<node CREATED="1426002118846" HGAP="19" ID="ID_667351128" MODIFIED="1426008507226" TEXT="producidos por " VSHIFT="-36">
+<node CREATED="1426003739744" ID="ID_683947330" MODIFIED="1428683015379" TEXT="Cambios en la energ&#xed;a t&#xe9;rmica de las mol&#xe9;culas">
+<node CREATED="1426002118846" HGAP="19" ID="ID_667351128" MODIFIED="1428682897396" TEXT="producidos por " VSHIFT="-36">
 <icon BUILTIN="idea"/>
-<node CREATED="1428427982262" ID="ID_1818644608" MODIFIED="1428428052675" STYLE="bubble" TEXT="- Cambios en la temperatura&#xa;- Cambios en la presi&#xf3;n&#xa;"/>
+<node CREATED="1426002130720" ID="ID_1225073670" MODIFIED="1428683008688" TEXT="Cambios en la temperatura"/>
+<node CREATED="1426002141391" ID="ID_1450678641" MODIFIED="1428683033991" TEXT="Cambios en la presi&#xf3;n">
+<arrowlink DESTINATION="ID_1450678641" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1816145243" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1450678641" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1816145243" SOURCE="ID_1450678641" STARTARROW="None" STARTINCLINATION="0;0;"/>
 </node>
-<node CREATED="1426003844152" ID="ID_680193211" MODIFIED="1426008484071" TEXT="al alcanzar">
+</node>
+<node CREATED="1426003844152" ID="ID_680193211" MODIFIED="1427564609415" TEXT="al alcanzar">
 <icon BUILTIN="idea"/>
-<node CREATED="1426004497442" ID="ID_1261677500" MODIFIED="1428427998070" TEXT="Temperaturas de cambio de fase">
+<node CREATED="1426004497442" ID="ID_1261677500" MODIFIED="1428682894621" TEXT="Temperaturas de cambio de fase">
 <node CREATED="1425950931014" ID="ID_1835141547" MODIFIED="1426008481513" TEXT="como">
 <icon BUILTIN="idea"/>
-<node CREATED="1428428006658" ID="ID_1722052974" MODIFIED="1428428052673" STYLE="bubble" TEXT="Punto de fusi&#xf3;n&#xa;Punto de ebullici&#xf3;n&#xa;Punto de condensaci&#xf3;n&#xa;Punto de fusi&#xf3;n&#xa;"/>
+<node CREATED="1428422505165" ID="ID_338761847" MODIFIED="1428682993752" STYLE="bubble" TEXT="- punto de fusi&#xf3;n&#xa;- Punto de congelaci&#xf3;n&#xa;- Punto de ebullici&#xf3;n&#xa;- Punto de condensaci&#xf3;n"/>
+</node>
 </node>
 </node>
 </node>
