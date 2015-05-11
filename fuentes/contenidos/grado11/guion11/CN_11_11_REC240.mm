e7c6633a8724277fd14dcfe5206d619f727bdd28
lyzbernal <lyzmarcela@gmail.com>
2015-05-11 10:59:14 -0500

    Mapa conceptual CN_11_11_CO
    Mapa conceptual

---
+++ fuentes/contenidos/grado11/guion11/CN_11_11_REC240.mm
@@ -0,0 +1,117 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1428467471443" ID="ID_1792704751" MODIFIED="1429815520180" TEXT="Los hidrocarburos">
+<node CREATED="1428467808718" ID="ID_945376979" MODIFIED="1431358555235" POSITION="right" TEXT="est&#xe1;n formados por">
+<icon BUILTIN="idea"/>
+<node CREATED="1428468581929" ID="ID_1089946680" MODIFIED="1429417071084" TEXT="Carbono"/>
+<node CREATED="1428468588089" ID="ID_1569090835" MODIFIED="1429417077761" TEXT="Hidr&#xf3;geno"/>
+<node CREATED="1428500773183" ID="ID_1457050495" MODIFIED="1428501432973" TEXT="algunas trazas de">
+<icon BUILTIN="idea"/>
+<node CREATED="1428500785898" HGAP="26" ID="ID_718988790" MODIFIED="1429417093353" VSHIFT="16">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(34, 34, 34)" size="3" face="SansSerif">Azufre </font>
+    </p>
+    <p>
+      <font color="rgb(34, 34, 34)" size="3" face="SansSerif">Nitr&#243;geno </font>
+    </p>
+    <p>
+      <font color="rgb(34, 34, 34)" size="3" face="SansSerif">Ox&#237;geno&#160; </font>
+    </p>
+    <p>
+      <font color="rgb(34, 34, 34)" size="3" face="SansSerif">Hierro </font>
+    </p>
+    <p>
+      <font color="rgb(34, 34, 34)" size="3" face="SansSerif">Plomo </font>
+    </p>
+    <p>
+      <font color="rgb(34, 34, 34)" size="3" face="SansSerif">N&#237;quel</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1428468640929" ID="ID_1760101713" MODIFIED="1429815561729" POSITION="right" TEXT="son derivados">
+<icon BUILTIN="idea"/>
+<node CREATED="1428468660915" ID="ID_1212972356" MODIFIED="1431358700085" TEXT="del petr&#xf3;leo"/>
+</node>
+<node CREATED="1425873944374" HGAP="13" ID="ID_1567150027" MODIFIED="1428470128558" POSITION="right" TEXT="pueden ser" VSHIFT="-16">
+<icon BUILTIN="idea"/>
+<node CREATED="1425873951682" HGAP="16" ID="ID_1842883943" MODIFIED="1429815655456" TEXT="de cadena abierta" VSHIFT="12">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1428468920692" ID="ID_1035354266" MODIFIED="1429417167182" TEXT="que se dividen en">
+<icon BUILTIN="idea"/>
+<node CREATED="1427386330924" ID="ID_1963237771" MODIFIED="1429417154530" TEXT="Saturados">
+<node CREATED="1428468989767" ID="ID_720468517" MODIFIED="1429815584513" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1427386368323" ID="ID_1367754519" MODIFIED="1429417180885" TEXT="Alcanos">
+<node CREATED="1428469051542" ID="ID_1136970348" MODIFIED="1429417204580" TEXT="con f&#xf3;rmula">
+<icon BUILTIN="idea"/>
+<node CREATED="1427386395742" ID="ID_29939795" MODIFIED="1427386418132" TEXT="CnH2n+2"/>
+</node>
+<node CREATED="1428469636982" ID="ID_1070377259" MODIFIED="1429417217433" TEXT="presentan">
+<icon BUILTIN="idea"/>
+<node CREATED="1428469648609" ID="ID_1286138004" MODIFIED="1431358644699" TEXT="enlace sencillo"/>
+</node>
+<node CREATED="1429417221992" ID="ID_37134860" MODIFIED="1429815624591" TEXT="su terminaci&#xf3;n ">
+<icon BUILTIN="idea"/>
+<node CREATED="1429417240914" ID="ID_80918942" MODIFIED="1431358654648" TEXT="-ano"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1427386361076" ID="ID_1760297910" MODIFIED="1429417153174" TEXT="Insaturados">
+<node CREATED="1428469032407" ID="ID_1443265321" MODIFIED="1431358625310" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1427386376594" ID="ID_139916962" MODIFIED="1429417184409" TEXT="Alquenos  ">
+<node CREATED="1428469061434" ID="ID_1114307297" MODIFIED="1429417249996" TEXT="con f&#xf3;rmula">
+<icon BUILTIN="idea"/>
+<node CREATED="1427386501865" ID="ID_1568650927" MODIFIED="1427386503223" TEXT="CnH2n"/>
+</node>
+<node CREATED="1428469667249" ID="ID_1561493584" MODIFIED="1429417258472" TEXT="presentan">
+<icon BUILTIN="idea"/>
+<node CREATED="1428469680073" ID="ID_1539616982" MODIFIED="1431358648132" TEXT="enlace doble"/>
+</node>
+<node CREATED="1429417264553" ID="ID_1242743235" MODIFIED="1429815662848" TEXT="su terminaci&#xf3;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1429417357971" ID="ID_390105748" MODIFIED="1431358657897" TEXT="-eno"/>
+</node>
+</node>
+<node CREATED="1427386481668" ID="ID_721612137" MODIFIED="1429417187734" TEXT="Alquinos ">
+<node CREATED="1428469065138" ID="ID_87519834" MODIFIED="1428469864002" TEXT="su f&#xf3;rmula">
+<icon BUILTIN="idea"/>
+<node CREATED="1427386494780" ID="ID_401635199" MODIFIED="1427386496718" TEXT="CnH2n-2"/>
+</node>
+<node CREATED="1428469690205" ID="ID_900356457" MODIFIED="1428469711096" TEXT="con">
+<icon BUILTIN="idea"/>
+<node CREATED="1428469694687" ID="ID_1466526567" MODIFIED="1431358638996" TEXT="enlace triple"/>
+</node>
+<node CREATED="1429417365727" ID="ID_1676837196" MODIFIED="1429815672801" TEXT="su terminaci&#xf3;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1429417376117" ID="ID_1385024131" MODIFIED="1431358668769" TEXT="-ino"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425873957340" HGAP="16" ID="ID_1168406158" MODIFIED="1427389741673" TEXT="alic&#xed;clicos" VSHIFT="-2">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1428468952709" ID="ID_902289397" MODIFIED="1429815743182" TEXT="como los">
+<icon BUILTIN="idea"/>
+<node CREATED="1427386706906" ID="ID_23520127" MODIFIED="1429417401691" TEXT="Cicloalcanos"/>
+<node CREATED="1428468813761" ID="ID_255460568" MODIFIED="1429417408078" TEXT="Cicloalquenos"/>
+<node CREATED="1428468820329" ID="ID_874424170" MODIFIED="1429417415254" TEXT="Cicloalquinos"/>
+</node>
+</node>
+<node CREATED="1425873960522" HGAP="17" ID="ID_1314303640" MODIFIED="1427389741673" TEXT="Arom&#xe1;ticos" VSHIFT="-10">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
