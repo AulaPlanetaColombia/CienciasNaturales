00b948ad67393336c918f235e9f1f8245f4f1c19
Maaljurem <miguelaljure@hotmail.com>
2015-06-29 11:59:09 -0500

    Originales de autor

---
+++ fuentes/contenidos/grado09/guion04/CN_09_04_CO_REC210.mm
@@ -0,0 +1,165 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1429478943981" ID="ID_846309099" MODIFIED="1429481058683" TEXT="La clasificaci&#xf3;n biol&#xf3;gica">
+<node CREATED="1429479776910" HGAP="22" ID="ID_1856640630" MODIFIED="1429482145494" POSITION="right" TEXT="es estudiada por" VSHIFT="-61">
+<icon BUILTIN="idea"/>
+<node CREATED="1429479788412" ID="ID_1965478719" MODIFIED="1429480776709" TEXT="La Taxonom&#xed;a">
+<node CREATED="1429479975719" ID="ID_1655383945" MODIFIED="1429482492872" TEXT="fundada por">
+<icon BUILTIN="idea"/>
+<node CREATED="1429480940135" ID="ID_1586830184" MODIFIED="1429482593153" TEXT="Carlos Linneo (1753)">
+<node CREATED="1429480977385" ID="ID_1090311869" MODIFIED="1429480986198" TEXT="quien propuso">
+<icon BUILTIN="idea"/>
+<node CREATED="1429480991948" ID="ID_516406200" MODIFIED="1429481002760" TEXT="El sistema binomial"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1429480338057" HGAP="49" ID="ID_1713585804" MODIFIED="1429482154119" POSITION="right" TEXT="facilita organizar a " VSHIFT="-31">
+<icon BUILTIN="idea"/>
+<node CREATED="1429480665649" ID="ID_950978580" MODIFIED="1429480671741" TEXT="Los seres vivos">
+<node CREATED="1429480676272" ID="ID_747737261" MODIFIED="1429480679444" TEXT="en">
+<icon BUILTIN="idea"/>
+<node CREATED="1429480681944" ID="ID_1803442567" MODIFIED="1429480692666" TEXT="Categor&#xed;as jer&#xe1;rquicas">
+<node CREATED="1429480695228" ID="ID_1593943493" MODIFIED="1429481277209" TEXT="entre ellas">
+<arrowlink DESTINATION="ID_1593943493" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1048560567" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1593943493" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1048560567" SOURCE="ID_1593943493" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1429480705459" ID="ID_1284454368" MODIFIED="1430707100476">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Dominio
+    </p>
+    <p>
+      Reino
+    </p>
+    <p>
+      Filo o Divisi&#243;n
+    </p>
+    <p>
+      Clase
+    </p>
+    <p>
+      Orden
+    </p>
+    <p>
+      Familia
+    </p>
+    <p>
+      G&#233;nero
+    </p>
+    <p>
+      Especie
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_1284454368" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_853659725" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1284454368" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_853659725" SOURCE="ID_1284454368" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1429481060074" HGAP="1" ID="ID_1890206334" MODIFIED="1429481749017" POSITION="right" TEXT="fue replanteada por" VSHIFT="-11">
+<arrowlink DESTINATION="ID_1890206334" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_29330042" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1890206334" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1056164744" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1890206334" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_29330042" SOURCE="ID_1890206334" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1890206334" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1056164744" SOURCE="ID_1890206334" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1429481094996" ID="ID_575639123" MODIFIED="1429481548817" TEXT="Woese en 1977">
+<arrowlink DESTINATION="ID_575639123" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1073648553" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_575639123" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1073648553" SOURCE="ID_575639123" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1429481108324" ID="ID_783315494" MODIFIED="1429481652099" TEXT="quien propuso">
+<arrowlink DESTINATION="ID_783315494" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_97142243" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_783315494" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_97142243" SOURCE="ID_783315494" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1429481428864" ID="ID_369264420" MODIFIED="1429481436957" TEXT="Tres dominios">
+<node CREATED="1429481438786" ID="ID_1269792577" MODIFIED="1429481446192" TEXT="estos son">
+<icon BUILTIN="idea"/>
+<node CREATED="1429481450176" ID="ID_124389323" MODIFIED="1429481481770">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Dominio Bacteria
+    </p>
+    <p>
+      Dominio Archaea
+    </p>
+    <p>
+      Dominio Eukarya
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1429481688970" HGAP="-6" ID="ID_302545785" MODIFIED="1429482125353" POSITION="right" TEXT="actualmente reconoce" VSHIFT="51">
+<icon BUILTIN="idea"/>
+<node CREATED="1429481715079" ID="ID_1844203611" MODIFIED="1430707139664" TEXT="Seis reinos">
+<node CREATED="1429481755532" ID="ID_1688219660" MODIFIED="1430707159601" TEXT="de los cuales">
+<icon BUILTIN="idea"/>
+<node CREATED="1429481767564" ID="ID_368494083" MODIFIED="1430707167430" TEXT="Dos son procariotas ">
+<node CREATED="1429481790769" ID="ID_1023811439" MODIFIED="1430707185820" TEXT="estos son">
+<icon BUILTIN="idea"/>
+<node CREATED="1429481801863" ID="ID_1633982315" MODIFIED="1430707210102">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Reino Eubacteria
+    </p>
+    <p>
+      Reino Archaeobacteria
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1429481768394" HGAP="28" ID="ID_1471543935" MODIFIED="1430707177008" TEXT="Cuatro son eucariotas" VSHIFT="22">
+<node CREATED="1429481903840" ID="ID_1201304113" MODIFIED="1430707221492" TEXT="estos son">
+<icon BUILTIN="idea"/>
+<node CREATED="1429481921949" ID="ID_698195633" MODIFIED="1430707270461">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Reino Protista
+    </p>
+    <p>
+      Reino Fungi
+    </p>
+    <p>
+      Reino Plantae
+    </p>
+    <p>
+      Reino Animalia
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ fuentes/contenidos/grado09/guion05/Mapa_conceptual_CN_09_05_CO.mm
@@ -0,0 +1,98 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1431361931534" ID="ID_1664423480" MODIFIED="1431362699490" TEXT="La taxonom&#xed;a moderna">
+<node CREATED="1431362718678" ID="ID_467493195" MODIFIED="1431365041173" POSITION="right" TEXT="tiene en cuenta">
+<icon BUILTIN="idea"/>
+<node CREATED="1431362731865" HGAP="42" ID="ID_1994130713" MODIFIED="1432588548821" TEXT="Los conceptos de especie" VSHIFT="-70">
+<node CREATED="1432588430915" ID="ID_56389858" MODIFIED="1432588576931" TEXT="entre ellos">
+<icon BUILTIN="idea"/>
+<node CREATED="1432588582650" ID="ID_1075466700" MODIFIED="1432588691056">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      El concepto biol&#243;gico
+    </p>
+    <p>
+      El concepto filogen&#233;tico
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1431363617826" HGAP="81" ID="ID_847759009" MODIFIED="1431365237418" TEXT="Los c&#xf3;digos interacionales de nomenclatura" VSHIFT="-49">
+<node CREATED="1431363645295" ID="ID_8665783" MODIFIED="1432588727525" TEXT="tales como">
+<icon BUILTIN="idea"/>
+<node CREATED="1432588732947" ID="ID_466032352" MODIFIED="1432588799917">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      El c&#243;digo de nomenclatura Zool&#243;gica&#160;
+    </p>
+    <p>
+      El c&#243;digo de nomenclatura Bot&#225;nica
+    </p>
+    <p>
+      El c&#243;digo de nomenclatura de Bacteris
+    </p>
+    <p>
+      El c&#243;digo de nomenclatura de Virus
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1431364340051" HGAP="124" ID="ID_1327312750" MODIFIED="1432588842542" TEXT="M&#xe9;todos y herramientas" VSHIFT="5">
+<node CREATED="1431364371448" ID="ID_1502537808" MODIFIED="1432588850729" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1431364379121" ID="ID_350595561" MODIFIED="1432588938058">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Las claves dicot&#243;micas&#160;
+    </p>
+    <p>
+      Las colecciones biol&#243;gicas
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1431364767035" HGAP="43" ID="ID_1986396466" MODIFIED="1431365226812" TEXT="El convenio de diversidad biol&#xf3;gica" VSHIFT="109">
+<node CREATED="1431364805311" ID="ID_398152780" MODIFIED="1431364819602" TEXT="cuyos prop&#xf3;sitos son">
+<icon BUILTIN="idea"/>
+<node CREATED="1431364931876" ID="ID_32119323" MODIFIED="1431364944782" TEXT="Conservar la biodiversidad"/>
+<node CREATED="1431364933262" ID="ID_1601929008" MODIFIED="1431364968379" TEXT="El uso sostenible de la biodiversidad"/>
+<node CREATED="1431364934038" ID="ID_1437773678" MODIFIED="1431365023782" TEXT="Beneficios equitativos derivados de la biodiversidad"/>
+</node>
+</node>
+<node CREATED="1431365114421" HGAP="26" ID="ID_431545898" MODIFIED="1431365222064" TEXT="Los caracteres moleculares" VSHIFT="119">
+<node CREATED="1431365137397" ID="ID_1690460773" MODIFIED="1431365277445" TEXT="que junto con">
+<icon BUILTIN="idea"/>
+<node CREATED="1431365161624" ID="ID_252860973" MODIFIED="1431365170008" TEXT="La bioinform&#xe1;tica">
+<arrowlink DESTINATION="ID_252860973" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1680789352" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_252860973" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1680789352" SOURCE="ID_252860973" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1431365174887" ID="ID_578884079" MODIFIED="1432589115496" TEXT="complementan">
+<icon BUILTIN="idea"/>
+<node CREATED="1431365186438" ID="ID_1044224572" MODIFIED="1431365202753" TEXT="La clasificaci&#xf3;n biol&#xf3;gica"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
