c94b98aab199d67b7d0249f4a10e46c2f87e32f8
Maaljurem <miguelaljure@hotmail.com>
2015-07-02 10:37:25 -0500

    Nueva escaleta 08_04
    Guion modificado para incluir los nuevos recursos. Mapa conceptual
    corregido.

--- fuentes/contenidos/grado08/guion04/Nuevo Mapa.mm
+++
@@ -1,4 +0,0 @@
-<map version="1.0.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1430512560433" ID="ID_334770341" MODIFIED="1430512560433" TEXT="Nuevo Mapa"/>
-</map>
