fa17083817bd01004137f6948a933b36e81c9bbb
Maaljurem <miguelaljure@hotmail.com>
2015-07-05 14:58:42 -0500

    Mapa simplificado

---
+++ fuentes/contenidos/grado08/guion06/Mapa conceptual simplificado CN_08_06_CO.mm
@@ -0,0 +1,163 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1426429769846" ID="ID_1849005272" MODIFIED="1435261555870" TEXT="Los genes">
+<node CREATED="1426431052059" HGAP="35" ID="ID_1431677298" MODIFIED="1435261583692" POSITION="left" TEXT="se localizan en" VSHIFT="-76">
+<icon BUILTIN="idea"/>
+<node CREATED="1426431069305" HGAP="33" ID="ID_1073839482" MODIFIED="1435261908356" TEXT="Los cromosomas" VSHIFT="-11">
+<node CREATED="1426431577980" ID="ID_1789585204" MODIFIED="1426431588442" TEXT="pueden presentar">
+<icon BUILTIN="idea"/>
+<node CREATED="1426431595556" HGAP="19" ID="ID_1428655668" MODIFIED="1435261717133" TEXT="Aberraciones" VSHIFT="18">
+<node CREATED="1426431603744" HGAP="19" ID="ID_1780588592" MODIFIED="1426431759440" TEXT="de tipo" VSHIFT="-18">
+<icon BUILTIN="idea"/>
+<node CREATED="1426431648107" ID="ID_1874650205" MODIFIED="1435261699561" TEXT="num&#xe9;rico">
+<node CREATED="1426431665311" HGAP="18" ID="ID_898922056" MODIFIED="1429476478313" TEXT="llamadas" VSHIFT="-24">
+<icon BUILTIN="idea"/>
+<node CREATED="1429476422724" ID="ID_180630180" MODIFIED="1435261967743">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Euploid&#237;as
+    </p>
+    <p>
+      -Aneuploid&#237;as
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1426431655119" ID="ID_1158469548" MODIFIED="1435261699381" TEXT="estructural">
+<node CREATED="1426431707079" HGAP="16" ID="ID_169341692" MODIFIED="1429476492894" TEXT="denominadas" VSHIFT="28">
+<icon BUILTIN="idea"/>
+<node CREATED="1426431715597" ID="ID_1197643335" MODIFIED="1435261984059">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Deleciones
+    </p>
+    <p>
+      -Duplicaciones
+    </p>
+    <p>
+      -Inversiones
+    </p>
+    <p>
+      -Traslocaciones
+    </p>
+    <p>
+      -Inserciones
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1426430881405" HGAP="19" ID="ID_1754179417" MODIFIED="1435261265192" POSITION="left" TEXT="son" VSHIFT="71">
+<icon BUILTIN="idea"/>
+<node CREATED="1426430906240" ID="ID_1737985839" MODIFIED="1435261281798" TEXT="Mol&#xe9;culas con informaci&#xf3;n gen&#xe9;tica">
+<node CREATED="1426430065139" ID="ID_72418037" MODIFIED="1436126104820" TEXT="constituyen">
+<arrowlink DESTINATION="ID_72418037" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1983438101" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_72418037" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1983438101" SOURCE="ID_72418037" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1426430102485" HGAP="22" ID="ID_1066158040" MODIFIED="1436126054633" TEXT="El genotipo" VSHIFT="-3">
+<node CREATED="1426430198232" HGAP="22" ID="ID_1778280223" MODIFIED="1436126038643" TEXT="que influye en" VSHIFT="1">
+<arrowlink DESTINATION="ID_1778280223" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1003574845" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1778280223" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1401870403" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1778280223" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1003574845" SOURCE="ID_1778280223" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1778280223" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1401870403" SOURCE="ID_1778280223" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1426430103676" HGAP="27" ID="ID_474201780" MODIFIED="1435261851061" TEXT="El fenotipo" VSHIFT="-2"/>
+</node>
+</node>
+</node>
+<node CREATED="1426430762428" HGAP="18" ID="ID_1115008483" MODIFIED="1435261806701" TEXT="se presentan en" VSHIFT="36">
+<icon BUILTIN="idea"/>
+<node CREATED="1426430788173" HGAP="21" ID="ID_739231426" MODIFIED="1435261890917" TEXT="Alelos" VSHIFT="7">
+<node CREATED="1426430793745" HGAP="31" ID="ID_680794931" MODIFIED="1435261893984" TEXT="que pueden ser" VSHIFT="13">
+<icon BUILTIN="idea"/>
+<node CREATED="1426430801443" HGAP="12" ID="ID_838377594" MODIFIED="1435261896804" VSHIFT="-14">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Dominantes
+    </p>
+    <p>
+      -Recesivos
+    </p>
+    <p>
+      -Codominantes
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1426431427183" HGAP="58" ID="ID_1341601087" MODIFIED="1436125814345" TEXT="se transmiten" VSHIFT="-31">
+<icon BUILTIN="idea"/>
+<node CREATED="1426431473813" HGAP="22" ID="ID_886773322" MODIFIED="1435261384851" TEXT="A trav&#xe9;s de las generaciones" VSHIFT="-35">
+<node CREATED="1426431511450" HGAP="17" ID="ID_1533922984" MODIFIED="1435261419677" TEXT="a trav&#xe9;s de" VSHIFT="-49">
+<icon BUILTIN="idea"/>
+<node CREATED="1426430801443" ID="ID_1413150151" MODIFIED="1435261434385">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -La mitosis
+    </p>
+    <p>
+      -La meiosis
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1426429914929" HGAP="18" ID="ID_626052686" MODIFIED="1435261875105" TEXT="Seg&#xfa;n las leyes de la herencia de Mendel" VSHIFT="34">
+<node CREATED="1429475892184" HGAP="13" ID="ID_402626123" MODIFIED="1435257874958" TEXT="que son" VSHIFT="31">
+<icon BUILTIN="idea"/>
+<node CREATED="1429475944692" ID="ID_327780515" MODIFIED="1435257893387">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Ley de la uniformidad
+    </p>
+    <p>
+      -Ley de la segregaci&#243;n&#160;
+    </p>
+    <p>
+      -Ley de la independencia de caracteres
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1436125921892" ID="ID_605628085" MODIFIED="1436125931190" TEXT="que indican">
+<icon BUILTIN="idea"/>
+<node CREATED="1436125932687" ID="ID_1993908856" MODIFIED="1436125953420" TEXT="Qu&#xe9; prote&#xed;nas se sintetizan"/>
+</node>
+</node>
+</node>
+</node>
+</map>
