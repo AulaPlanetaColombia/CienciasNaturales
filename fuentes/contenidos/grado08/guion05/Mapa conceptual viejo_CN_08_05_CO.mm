448b429b192c04ed408bf59f17a1f45cf822cfc0
Maaljurem <miguelaljure@hotmail.com>
2015-07-06 17:37:16 -0500

    Arreglos en la numeración de los recursos

---
+++ fuentes/contenidos/grado08/guion05/Mapa conceptual viejo_CN_08_05_CO.mm
@@ -0,0 +1,151 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425958268640" ID="ID_712647857" MODIFIED="1426464484676" TEXT="LA REPRODUCCI&#xd3;N HUMANA">
+<node CREATED="1425958338982" HGAP="9" ID="ID_947453642" MODIFIED="1428440123430" POSITION="right" TEXT="es una forma de" VSHIFT="-113">
+<icon BUILTIN="idea"/>
+<node CREATED="1425958360944" HGAP="15" ID="ID_280617822" MODIFIED="1425959226624" TEXT="Reproducci&#xf3;n sexual" VSHIFT="-22">
+<node CREATED="1425958433069" HGAP="14" ID="ID_644570528" MODIFIED="1428440145130" TEXT="realizada por medio de" VSHIFT="-30">
+<icon BUILTIN="idea"/>
+<node CREATED="1425958643756" HGAP="12" ID="ID_427278549" MODIFIED="1428440160637" TEXT="Sistemas reproductivos" VSHIFT="-29">
+<node CREATED="1425958700688" HGAP="10" ID="ID_1951277785" MODIFIED="1428440167002" TEXT="puedes ser" VSHIFT="-23">
+<icon BUILTIN="idea"/>
+<node CREATED="1425958712703" ID="ID_273661606" MODIFIED="1425958717883" TEXT="Masculino">
+<node CREATED="1425958769100" ID="ID_1445830003" MODIFIED="1425958778335" TEXT="compuesto por">
+<icon BUILTIN="idea"/>
+<node CREATED="1428421829036" ID="ID_854222328" MODIFIED="1428421984849" STYLE="bubble" TEXT="- Escroto&#xa;- Pene&#xa;- Epid&#xed;dimo&#xa;- Conducto deferente&#xa;- Conductos eyaculadores&#xa;- Uretra&#xa;- Ves&#xed;culas seminales&#xa;- Pr&#xf3;stata&#xa;- Gl&#xe1;ndulas bulbouretrales&#xa;"/>
+</node>
+</node>
+<node CREATED="1425958732352" ID="ID_390927934" MODIFIED="1425958736689" TEXT="Femenino">
+<node CREATED="1425958921112" ID="ID_805710027" MODIFIED="1425958928630" TEXT="compuesto por">
+<icon BUILTIN="idea"/>
+<node CREATED="1428421861751" ID="ID_1966577065" MODIFIED="1428421984849" STYLE="bubble" TEXT="- Vulva&#xa;- Monte de Venus&#xa;- Labios mayores y menores&#xa;- Cl&#xed;toris&#xa;- Ovarios&#xa;- Trompas de Falopio&#xa;- &#xda;tero&#xa;- Vagina&#xa;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425959208808" HGAP="56" ID="ID_705463619" MODIFIED="1428440208676" TEXT="que requiere" VSHIFT="-69">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959230857" ID="ID_667553363" MODIFIED="1425959245385" TEXT="Gametos sexuales">
+<node CREATED="1425959250282" ID="ID_1965995613" MODIFIED="1428440224370" TEXT="del">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959267288" ID="ID_1913173445" MODIFIED="1428440239876" TEXT="Hombre">
+<node CREATED="1425959291919" ID="ID_1331962857" MODIFIED="1426974427182" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959303222" ID="ID_404681138" MODIFIED="1426974407884" TEXT="Espermatozoides"/>
+</node>
+</node>
+</node>
+<node CREATED="1425959276201" ID="ID_1679315524" MODIFIED="1428440248550" TEXT="de la">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959283115" ID="ID_1522796229" MODIFIED="1428440254134" TEXT="Mujer">
+<node CREATED="1425959314907" ID="ID_542005714" MODIFIED="1426974432695" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959328536" ID="ID_131197814" MODIFIED="1426974418475" TEXT="&#xd3;vulos"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425959537929" ID="ID_1810981646" MODIFIED="1428440276287" TEXT="que presenta">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959581014" ID="ID_552366862" MODIFIED="1426974459886" TEXT="Ciclos reproductores">
+<node CREATED="1425959608765" ID="ID_1580222583" MODIFIED="1425959614166" TEXT="en">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959625726" ID="ID_546533131" MODIFIED="1428440295553" TEXT="Hombres">
+<node CREATED="1425959640836" ID="ID_1132721219" MODIFIED="1428440335262" TEXT="en donde">
+<icon BUILTIN="idea"/>
+<node CREATED="1428422175960" ID="ID_739731325" MODIFIED="1428440367991" STYLE="bubble" TEXT="- La producci&#xf3;n de espermatozoides es constante&#xa;- La eyaculaci&#xf3;n es peri&#xf3;dica&#xa;"/>
+</node>
+</node>
+</node>
+<node CREATED="1425959615989" ID="ID_1553186716" MODIFIED="1425959620101" TEXT="en">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959633746" ID="ID_571715184" MODIFIED="1428440296645" TEXT="Mujeres">
+<node CREATED="1426976390983" ID="ID_438669953" MODIFIED="1426976428068" TEXT="llamado">
+<icon BUILTIN="idea"/>
+<node CREATED="1426976395320" ID="ID_551789064" MODIFIED="1426976407991" TEXT="Ciclo menstrual">
+<node CREATED="1425959647930" ID="ID_541353722" MODIFIED="1426976445680" TEXT="que incluye">
+<icon BUILTIN="idea"/>
+<node CREATED="1425959840654" ID="ID_534521565" MODIFIED="1428440413492" TEXT="Fases">
+<node CREATED="1425959933393" ID="ID_953522738" MODIFIED="1425959938442" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428422197222" ID="ID_427230654" MODIFIED="1428422331537" STYLE="bubble" TEXT="- Menstruaci&#xf3;n&#xa;- Preovulaci&#xf3;n&#xa;- Ovulaci&#xf3;n&#xa;- Posovulaci&#xf3;n&#xa;"/>
+</node>
+</node>
+</node>
+<node CREATED="1425960002372" HGAP="19" ID="ID_904762538" MODIFIED="1428440408578" TEXT="regulado por" VSHIFT="35">
+<icon BUILTIN="idea"/>
+<node CREATED="1425960037819" ID="ID_1697772054" MODIFIED="1428440428336" TEXT="Hormonas">
+<node CREATED="1425960066299" ID="ID_861648654" MODIFIED="1425960075967" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428422244337" ID="ID_978635411" MODIFIED="1428422331537" STYLE="bubble" TEXT="- GnRH&#xa;- LH&#xa;- FSH&#xa;- Estr&#xf3;genos&#xa;- Progesterona&#xa;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425960222324" ID="ID_803209436" MODIFIED="1428440486905" TEXT="que puede ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1425960236437" ID="ID_1634022510" MODIFIED="1428440497372" TEXT="Natural">
+<node CREATED="1425960248727" ID="ID_1712944550" MODIFIED="1428440510118" TEXT="que incluye">
+<icon BUILTIN="idea"/>
+<node CREATED="1428422273252" ID="ID_1121455121" MODIFIED="1428422331537" STYLE="bubble" TEXT="- Fecundaci&#xf3;n&#xa;- Implantaci&#xf3;n&#xa;- Embarazo&#xa;- Parto&#xa;"/>
+</node>
+</node>
+<node CREATED="1425960237380" ID="ID_1057777362" MODIFIED="1428440497435" TEXT="Asistida">
+<node CREATED="1425960294954" ID="ID_548743617" MODIFIED="1425960303970" TEXT="mediante">
+<icon BUILTIN="idea"/>
+<node CREATED="1428422291206" ID="ID_1207287203" MODIFIED="1428422407557" STYLE="bubble" TEXT="- Inseminaci&#xf3;n artificial&#xa;- Fecundaci&#xf3;n in vitro"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1425958350105" ID="ID_1314229046" MODIFIED="1426976079592" POSITION="right" TEXT="se relaciona con">
+<icon BUILTIN="idea"/>
+<node CREATED="1425958370872" HGAP="23" ID="ID_1458656693" MODIFIED="1427325238347" TEXT="La sexualidad" VSHIFT="8">
+<node CREATED="1425960415628" ID="ID_1046900240" MODIFIED="1428440566683" TEXT="que requiere">
+<icon BUILTIN="idea"/>
+<node CREATED="1425960427047" ID="ID_1544111487" MODIFIED="1428440588414" TEXT="H&#xe1;bitos saludables"/>
+</node>
+<node CREATED="1425960472991" HGAP="48" ID="ID_1718660594" MODIFIED="1428440605886" TEXT="que implica" VSHIFT="144">
+<icon BUILTIN="idea"/>
+<node CREATED="1425960485184" HGAP="24" ID="ID_203300746" MODIFIED="1428440623046" TEXT="Caracteres sexuales primarios" VSHIFT="-28"/>
+<node CREATED="1425960500640" HGAP="30" ID="ID_1805329524" MODIFIED="1428440631455" TEXT="Caracteres sexuales secundarios" VSHIFT="46"/>
+</node>
+<node CREATED="1427325239837" HGAP="22" ID="ID_48077969" MODIFIED="1428440770317" TEXT="que incluye" VSHIFT="89">
+<icon BUILTIN="idea"/>
+<node CREATED="1425960580977" HGAP="37" ID="ID_644317344" MODIFIED="1428440788038" TEXT="La etapa activa reproductiva" VSHIFT="7">
+<node CREATED="1425960601795" ID="ID_336728794" MODIFIED="1428440808334" TEXT="que comienza en">
+<icon BUILTIN="idea"/>
+<node CREATED="1425960620371" ID="ID_994826326" MODIFIED="1428440654012" TEXT="La pubertad"/>
+</node>
+<node CREATED="1425960639073" ID="ID_464191570" MODIFIED="1428440943435" TEXT="se puede controlar con">
+<icon BUILTIN="idea"/>
+<node CREATED="1425960659620" ID="ID_440334715" MODIFIED="1428440661344" TEXT="M&#xe9;todos anticonceptivos">
+<node CREATED="1425960669459" ID="ID_674102852" MODIFIED="1428440936103" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428422505165" ID="ID_1123024789" MODIFIED="1429722956810" STYLE="bubble" TEXT="- M&#xe9;todos de barrera o mec&#xe1;nicos&#xa;- Hormonales&#xa;- Inhibidores de la implantaci&#xf3;n&#xa;- Esterilizaci&#xf3;n&#xa;- M&#xe9;todos naturales&#xa;- M&#xe9;todos qu&#xed;micos"/>
+</node>
+</node>
+</node>
+<node CREATED="1425960817389" ID="ID_421620042" MODIFIED="1428440809863" TEXT="que obliga a la prevenci&#xf3;n de">
+<icon BUILTIN="idea"/>
+<node CREATED="1425960848369" ID="ID_736544064" MODIFIED="1428440722237" TEXT="Enfermedades de transmisi&#xf3;n sexual">
+<node CREATED="1425960871081" ID="ID_180253432" MODIFIED="1425960874955" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428422697002" ID="ID_343607186" MODIFIED="1428422734482" STYLE="bubble" TEXT="- S&#xed;filis&#xa;- Clamidiasis&#xa;- Gonorrea&#xa;- Herpes genital&#xa;- Verrugas genitales&#xa;- Hepatitis B&#xa;- SIDA&#xa;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
