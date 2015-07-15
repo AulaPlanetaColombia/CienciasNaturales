4b6efb53c300242f5a2bf7fd106a7d7b7ff3021f
Maaljurem <miguelaljure@hotmail.com>
2015-07-15 12:50:14 -0500

    Mapa simplificado

---
+++ fuentes/contenidos/grado08/guion07/Mapa conceptual simplificado CN_08_07_CO.mm
@@ -0,0 +1,235 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1428067671789" ID="ID_380782859" MODIFIED="1428069993403">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      LA GEN&#201;TICA DESPU&#201;S DE MENDEL
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1428067780364" HGAP="40" ID="ID_1480357982" MODIFIED="1436982214504" POSITION="right" TEXT="incluye" VSHIFT="-4">
+<arrowlink DESTINATION="ID_1480357982" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_366159039" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1480357982" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_366159039" SOURCE="ID_1480357982" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1428067812455" HGAP="42" ID="ID_1905606110" MODIFIED="1436982330654" TEXT="La teor&#xed;a cromos&#xf3;mica de la herencia">
+<node CREATED="1428068610341" HGAP="31" ID="ID_1169050016" MODIFIED="1436982332994" TEXT="propuesta por" VSHIFT="-4">
+<icon BUILTIN="idea"/>
+<node CREATED="1428068622315" ID="ID_1435475056" MODIFIED="1428068633367" TEXT="Sutton y Boveri">
+<node CREATED="1428068636520" ID="ID_1688102373" MODIFIED="1436981181942" TEXT="plantea que">
+<icon BUILTIN="idea"/>
+<node CREATED="1428068646904" ID="ID_1785899863" MODIFIED="1436981220724">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Los genes est&#225;n en los cromosomas
+    </p>
+    <p>
+      -Genes cercanos pueden estar ligados
+    </p>
+    <p>
+      -Genes de un mismo cromosoma pueden heredarse por separado
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_1785899863" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1966760402" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1785899863" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1966760402" SOURCE="ID_1785899863" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1436981290503" ID="ID_86985560" MODIFIED="1436981295697" POSITION="left" TEXT="estudia">
+<icon BUILTIN="idea"/>
+<node CREATED="1428067815763" HGAP="31" ID="ID_1755727495" MODIFIED="1436982051628" TEXT="Enfermedades gen&#xe9;ticas" VSHIFT="7">
+<node CREATED="1428069460141" ID="ID_397005251" MODIFIED="1428898146770" TEXT="causadas por">
+<icon BUILTIN="idea"/>
+<node CREATED="1428069466882" ID="ID_1500781105" MODIFIED="1435714844290" TEXT="Mutaciones">
+<node CREATED="1428069477605" HGAP="21" ID="ID_448710723" MODIFIED="1436980794191" TEXT="pueden ser" VSHIFT="-53">
+<icon BUILTIN="idea"/>
+<node CREATED="1428069484838" ID="ID_1505806796" MODIFIED="1428898594561" TEXT="Hereditarias">
+<node CREATED="1428069509142" ID="ID_247969387" MODIFIED="1428898221789" TEXT="de tipo">
+<icon BUILTIN="idea"/>
+<node CREATED="1428898226201" ID="ID_1878319851" MODIFIED="1436980852597">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Autos&#243;mica dominante
+    </p>
+    <p>
+      -Autos&#243;mica recesiva
+    </p>
+    <p>
+      -Dominante ligada a X
+    </p>
+    <p>
+      -Recesiva ligada a X
+    </p>
+    <p>
+      -Mitocondrial
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1428069486314" HGAP="24" ID="ID_113355239" MODIFIED="1436980925075" TEXT="No hereditarias" VSHIFT="21">
+<node CREATED="1428069515671" ID="ID_621661388" MODIFIED="1428069522434" TEXT="si">
+<icon BUILTIN="idea"/>
+<node CREATED="1428069539064" ID="ID_17679877" MODIFIED="1436980944466" TEXT="El gen alterado est&#xe1; en las c&#xe9;lulas som&#xe1;tica"/>
+</node>
+</node>
+</node>
+<node CREATED="1428898325490" ID="ID_1233713686" MODIFIED="1428898631641" TEXT="originadas de manera">
+<icon BUILTIN="idea"/>
+<node CREATED="1428898341278" ID="ID_1946239877" MODIFIED="1428898636204" TEXT="Espont&#xe1;nea"/>
+<node CREATED="1428898439980" ID="ID_1273569716" MODIFIED="1428898641068" TEXT="Inducida">
+<node CREATED="1428898516945" ID="ID_288012924" MODIFIED="1428898529136" TEXT="por factores">
+<icon BUILTIN="idea"/>
+<node CREATED="1428898532011" ID="ID_1739276594" MODIFIED="1436980977522">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -F&#237;sicos
+    </p>
+    <p>
+      -Qu&#237;micos
+    </p>
+    <p>
+      -Biol&#243;gicos
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1436981435411" HGAP="23" ID="ID_733227868" MODIFIED="1436981479247" TEXT="diagnosticadas con" VSHIFT="31">
+<icon BUILTIN="idea"/>
+<node CREATED="1428067816499" HGAP="73" ID="ID_1254667055" MODIFIED="1436981481182" TEXT="Pruebas de diagn&#xf3;stico gen&#xe9;tico" VSHIFT="27">
+<node CREATED="1428069670615" ID="ID_1324040521" MODIFIED="1436982191002" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428898754920" ID="ID_1386251642" MODIFIED="1428898772221" TEXT="Prueba de dermatoglifos"/>
+<node CREATED="1428898756059" HGAP="28" ID="ID_1712143090" MODIFIED="1436980995924" TEXT="Cariotipaje" VSHIFT="30"/>
+<node CREATED="1428898756739" ID="ID_516020744" MODIFIED="1428982744750" TEXT="Pruebas de diagn&#xf3;stico prenatal">
+<node CREATED="1428982662091" ID="ID_1155065821" MODIFIED="1428982667392" TEXT="de tipo">
+<icon BUILTIN="idea"/>
+<node CREATED="1428982670502" ID="ID_1279996427" MODIFIED="1428982833663" TEXT="Invasivas">
+<node CREATED="1428982728789" ID="ID_1315254112" MODIFIED="1428982733759" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428982751370" ID="ID_1823855498" MODIFIED="1436981018871">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Ecograf&#237;a
+    </p>
+    <p>
+      -An&#225;lisis de sangre materna
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1428982671881" ID="ID_1768980263" MODIFIED="1428982839461" TEXT="No invasivas">
+<arrowlink DESTINATION="ID_1768980263" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_489209534" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1768980263" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_489209534" SOURCE="ID_1768980263" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1428982783284" ID="ID_398303596" MODIFIED="1428982789380" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1428982793013" ID="ID_1341603675" MODIFIED="1436981046390">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -An&#225;isis de cori&#243;n
+    </p>
+    <p>
+      -Amniocentesis
+    </p>
+    <p>
+      -Cordocentesis
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1428067815115" HGAP="40" ID="ID_1376072595" MODIFIED="1436982326614" TEXT="Otros tipos de herencia" VSHIFT="-8">
+<node CREATED="1428069022641" HGAP="49" ID="ID_101200937" MODIFIED="1436982159143" TEXT="como" VSHIFT="-1">
+<icon BUILTIN="idea"/>
+<node CREATED="1428069046552" HGAP="39" ID="ID_1161604543" MODIFIED="1436982265719" VSHIFT="-42">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -La herencia monog&#233;nica
+    </p>
+    <p>
+      -La herencia polig&#233;nica
+    </p>
+    <p>
+      -La herencia polial&#233;lica
+    </p>
+    <p>
+      -La epistasis
+    </p>
+    <p>
+      -La pleiotrop&#237;a
+    </p>
+    <p>
+      -La herencia materna
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1436982254518" HGAP="50" ID="ID_251619011" MODIFIED="1436982268465" TEXT="como" VSHIFT="46">
+<icon BUILTIN="idea"/>
+<node CREATED="1428067814428" HGAP="51" ID="ID_363622397" MODIFIED="1436982270820" TEXT="La herencia ligada al sexo">
+<node CREATED="1428068781426" ID="ID_1985129165" MODIFIED="1436981136645" TEXT="participan">
+<icon BUILTIN="idea"/>
+<node CREATED="1428068788893" HGAP="27" ID="ID_1433118244" MODIFIED="1436982275360" TEXT="Cromosomas sexuales" VSHIFT="-2">
+<node CREATED="1428068799431" ID="ID_1207907313" MODIFIED="1428897856517" TEXT="estos son">
+<arrowlink DESTINATION="ID_1207907313" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1867311068" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1207907313" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1867311068" SOURCE="ID_1207907313" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1428068816333" ID="ID_1994611895" MODIFIED="1428897875921" TEXT="XX en mujeres"/>
+<node CREATED="1428897866140" ID="ID_543215723" MODIFIED="1428897886507" TEXT="XY en hombres"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
