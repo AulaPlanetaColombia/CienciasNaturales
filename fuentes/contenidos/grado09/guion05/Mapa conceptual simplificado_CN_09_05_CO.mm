0f2271aa0575a69f1a4c859911263011d4a2177b
Maaljurem <miguelaljure@hotmail.com>
2015-07-30 15:29:17 -0500

    Escaleta grupo 1 con anotaciones y mapa 09_05

---
+++ fuentes/contenidos/grado09/guion05/Mapa conceptual simplificado_CN_09_05_CO.mm
@@ -0,0 +1,146 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1431361931534" ID="ID_1664423480" MODIFIED="1431362699490" TEXT="La taxonom&#xed;a moderna">
+<node CREATED="1438285039040" ID="ID_466280725" MODIFIED="1438285044096" POSITION="right" TEXT="permite">
+<icon BUILTIN="idea"/>
+<node CREATED="1438285045446" ID="ID_1843321189" MODIFIED="1438285090302">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Recolectar informaci&#243;n de manera organizada
+    </p>
+    <p>
+      -Manejar un lenguaje com&#250;n entre la comunidad cient&#237;fica
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1438285059652" ID="ID_1636405399" MODIFIED="1438285063400" TEXT="lo cual">
+<icon BUILTIN="idea"/>
+<node CREATED="1438285095063" ID="ID_368718213" MODIFIED="1438285113486" TEXT="Facilita el estudio de los seres vivos">
+<node CREATED="1438285113488" ID="ID_1671184705" MODIFIED="1438285214894" TEXT="permitiendo">
+<icon BUILTIN="idea"/>
+<node CREATED="1438285119837" ID="ID_874333970" MODIFIED="1438285167609">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Monitorear el estado de las poblaciones naturales
+    </p>
+    <p>
+      -Tomar medidas para la protecci&#243;n de los distintos organismos
+    </p>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_874333970" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_17240908" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_874333970" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_17240908" SOURCE="ID_874333970" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1438284836437" ID="ID_428759053" MODIFIED="1438284843075" POSITION="left" TEXT="emplea">
+<icon BUILTIN="idea"/>
+<node CREATED="1438284844208" HGAP="28" ID="ID_603148878" MODIFIED="1438284972525" TEXT="Conceptos" VSHIFT="-47">
+<node CREATED="1438284851302" ID="ID_957716614" MODIFIED="1438284854482" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1438284855533" ID="ID_1989884564" MODIFIED="1438284868748" TEXT="La definici&#xf3;n de especie">
+<node CREATED="1438284868749" ID="ID_1899433272" MODIFIED="1438284874440" TEXT="destacando">
+<icon BUILTIN="idea"/>
+<node CREATED="1438284875477" ID="ID_978020839" MODIFIED="1438284899004">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -La especie biol&#243;gica
+    </p>
+    <p>
+      -La especie filogen&#233;tica
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1438284905068" ID="ID_28736578" MODIFIED="1438284909034" TEXT="M&#xe9;todos">
+<node CREATED="1438284909036" ID="ID_1325028889" MODIFIED="1438284911800" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1438284912716" ID="ID_591835994" MODIFIED="1438284926473" TEXT="El m&#xe9;todo para clasificar organismos nuevos"/>
+<node CREATED="1438284928397" ID="ID_1300428011" MODIFIED="1438284939540" TEXT="El m&#xe9;todo para nombrar organismos nuevos">
+<node CREATED="1438284939542" ID="ID_1496077101" MODIFIED="1438284944107" TEXT="seg&#xfa;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1431363617826" HGAP="49" ID="ID_847759009" MODIFIED="1438284957009" TEXT="Los c&#xf3;digos interacionales de nomenclatura" VSHIFT="14">
+<node CREATED="1431363645295" ID="ID_8665783" MODIFIED="1438275985081" TEXT="son">
+<arrowlink DESTINATION="ID_8665783" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1430190771" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_8665783" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1430190771" SOURCE="ID_8665783" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1432588732947" ID="ID_466032352" MODIFIED="1438284968279">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -El c&#243;digo de nomenclatura Zool&#243;gica&#160;
+    </p>
+    <p>
+      -El c&#243;digo de nomenclatura Bot&#225;nica
+    </p>
+    <p>
+      -El c&#243;digo de nomenclatura de Bacteris
+    </p>
+    <p>
+      -El c&#243;digo de nomenclatura de Virus
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1438284977486" ID="ID_728287389" MODIFIED="1438284986627" TEXT="Herramientas">
+<node CREATED="1438284987886" ID="ID_1186648254" MODIFIED="1438284990452" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1431364379121" ID="ID_350595561" MODIFIED="1438285018935">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Las claves dicot&#243;micas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1431364379121" ID="ID_1943920755" MODIFIED="1438285027056">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Las claves dicot&#243;micas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
