96ee7e74b75c89c32c782577dbc05c52743a2be4
ggcuervov <ggcuervo@gmail.com>
2015-08-10 12:19:21 -0500

    Escaletas y mapas conceptuales
    Temas CN_07_07_CO, CN_07_08_CO, CN_08_08_CO, CN_09_07_CO, CN_09_08_CO.

---
+++ fuentes/contenidos/grado09/guion08/Mapa conceptual CN_09_08_CO.mm
@@ -0,0 +1,34 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1439070915277" ID="ID_617073034" MODIFIED="1439071571638" TEXT="La distribuci&#xf3;n de los seres vivos en el planeta">
+<node CREATED="1439071512116" ID="ID_697826819" MODIFIED="1439073303681" POSITION="right" TEXT="es estudiada por">
+<icon BUILTIN="idea"/>
+<node CREATED="1439071693172" ID="ID_986360393" MODIFIED="1439072771156" TEXT="La Biogeograf&#xed;a"/>
+</node>
+<node CREATED="1439071926601" ID="ID_1743558675" MODIFIED="1439073060532" POSITION="right" TEXT="se relaciona con">
+<icon BUILTIN="idea"/>
+<node CREATED="1439071940741" ID="ID_1283498061" MODIFIED="1439073588897" TEXT="Los centros de origen de las especies"/>
+<node CREATED="1439071969261" ID="ID_556187020" MODIFIED="1439073602022" TEXT="Las zonas de distribuci&#xf3;n de las especies">
+<node CREATED="1439072056558" ID="ID_420565537" MODIFIED="1439073077907" TEXT="determinadas por ">
+<icon BUILTIN="idea"/>
+<node CREATED="1439072071948" ID="ID_1960116963" MODIFIED="1439072107565" TEXT="Factores hist&#xf3;ricos">
+<node CREATED="1439072110565" ID="ID_985533652" MODIFIED="1439073082735" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1439072184288" ID="ID_657350851" MODIFIED="1439072283040" TEXT="La dispersi&#xf3;n"/>
+<node CREATED="1439072211531" ID="ID_1604989671" MODIFIED="1439072289931" TEXT="La deriva continental"/>
+<node CREATED="1439072231651" ID="ID_1424987537" MODIFIED="1439072297431" TEXT="El surgimiento de las cordilleras"/>
+<node CREATED="1439072255214" ID="ID_384742851" MODIFIED="1439072306185" TEXT="Las glaciaciones"/>
+</node>
+</node>
+<node CREATED="1439072075792" ID="ID_698422598" MODIFIED="1439072367165" TEXT="La competencia"/>
+<node CREATED="1439072371009" ID="ID_237237480" MODIFIED="1439072379243" TEXT="Factores bi&#xf3;ticos"/>
+</node>
+</node>
+</node>
+<node CREATED="1439072456836" ID="ID_1402625657" MODIFIED="1439073069313" POSITION="right" TEXT="se representa mediante">
+<icon BUILTIN="idea"/>
+<node CREATED="1439072471974" ID="ID_321433302" MODIFIED="1439226001078" TEXT="Las regiones biogeogr&#xe1;ficas"/>
+<node CREATED="1439072586294" ID="ID_1948464469" MODIFIED="1439226018929" TEXT="Las regiones ecol&#xf3;gicas"/>
+</node>
+</node>
+</map>
