fe978b16bb0f7574f52c049c487589ac275ffed7
Maaljurem <miguelaljure@hotmail.com>
2015-08-16 14:05:38 -0500

    Mapa conceptual CN_08_13_CO

---
+++ fuentes/contenidos/grado08/guion13/mapaconceptual CN_08_13_CO.mm
@@ -0,0 +1,100 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1439729397911" ID="ID_1081295287" MODIFIED="1439751664293" TEXT="La comunicaci&#xf3;n">
+<node CREATED="1439729489185" HGAP="31" ID="ID_314597739" MODIFIED="1439750831954" POSITION="right" TEXT="tiene" VSHIFT="-6">
+<icon BUILTIN="idea"/>
+<node CREATED="1439729662619" ID="ID_514281655" MODIFIED="1439750866508" TEXT="Elementos clave">
+<node CREATED="1439729666311" ID="ID_1339976252" MODIFIED="1439750773188" TEXT="son">
+<icon BUILTIN="idea"/>
+<node CREATED="1439729668138" ID="ID_22446571" MODIFIED="1439729728911">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Emisor
+    </p>
+    <p>
+      -Receptor
+    </p>
+    <p>
+      -Mensaje
+    </p>
+    <p>
+      -C&#243;digo
+    </p>
+    <p>
+      -Canal de transmisi&#243;n
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1439730235083" ID="ID_1994720642" MODIFIED="1439751532669" POSITION="left" TEXT="se sirve de">
+<icon BUILTIN="idea"/>
+<node CREATED="1439751464815" ID="ID_1054496026" MODIFIED="1439751470759" TEXT="La tecnolog&#xed;a">
+<node CREATED="1439751470759" ID="ID_484832360" MODIFIED="1439751515938" TEXT="para">
+<icon BUILTIN="idea"/>
+<node CREATED="1439730598583" ID="ID_1640750913" MODIFIED="1439751577128" TEXT="Aumentar su alcance en tiempo y espacio"/>
+</node>
+</node>
+</node>
+<node CREATED="1439750572714" HGAP="23" ID="ID_226542070" MODIFIED="1439750757526" POSITION="right" TEXT="puede ser" VSHIFT="54">
+<icon BUILTIN="idea"/>
+<node CREATED="1439750593041" HGAP="15" ID="ID_1311584454" MODIFIED="1439750783500" TEXT="Unidireccional" VSHIFT="-20">
+<node CREATED="1439750602963" ID="ID_63909113" MODIFIED="1439750873886" TEXT="como en">
+<icon BUILTIN="idea"/>
+<node CREATED="1439750608610" ID="ID_278126858" MODIFIED="1439750686969">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Las cartas
+    </p>
+    <p>
+      -Los medios impresos
+    </p>
+    <p>
+      -La radio
+    </p>
+    <p>
+      -El cine
+    </p>
+    <p>
+      -La televisi&#243;n
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1439750695284" HGAP="16" ID="ID_980973339" MODIFIED="1439750792532" TEXT="Bidireccional" VSHIFT="12">
+<node CREATED="1439750720585" ID="ID_392817457" MODIFIED="1439750725733" TEXT="como es">
+<icon BUILTIN="idea"/>
+<node CREATED="1439750728026" ID="ID_883867313" MODIFIED="1439750754047">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -La telefon&#237;a (fija y m&#243;vil)
+    </p>
+    <p>
+      -Internet
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
