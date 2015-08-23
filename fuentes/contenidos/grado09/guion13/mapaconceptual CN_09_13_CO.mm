d06e13b855e3f2abee07d11dc8018c8f2d534861
Maaljurem <miguelaljure@hotmail.com>
2015-08-23 17:49:59 -0500

    Escaleta y mapa conceptual CN_09_13_CO

---
+++ fuentes/contenidos/grado09/guion13/mapaconceptual CN_09_13_CO.mm
@@ -0,0 +1,145 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1439729397911" ID="ID_1081295287" MODIFIED="1440175383039" TEXT="La tecnolog&#xed;a para transformar la energ&#xed;a">
+<node CREATED="1440308314206" ID="ID_1027352564" MODIFIED="1440310221399" POSITION="right" TEXT="posibilita">
+<arrowlink DESTINATION="ID_1027352564" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1381964012" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1027352564" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1381964012" SOURCE="ID_1027352564" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1440308344895" ID="ID_830072648" MODIFIED="1440309572037" TEXT="Convertir alguna forma de energ&#xed;a en otra que se necesite">
+<node CREATED="1440310223864" ID="ID_615398356" MODIFIED="1440310282209" TEXT="con base en">
+<icon BUILTIN="idea"/>
+<node CREATED="1440310283952" ID="ID_124005470" MODIFIED="1440310413728" TEXT="El principio de conservaci&#xf3;n de la energ&#xed;a"/>
+</node>
+</node>
+</node>
+<node CREATED="1440308365820" HGAP="-18" ID="ID_1680789737" MODIFIED="1440310412532" POSITION="right" TEXT="permite obtener" VSHIFT="98">
+<icon BUILTIN="idea"/>
+<node CREATED="1440308491656" HGAP="25" ID="ID_327853172" MODIFIED="1440309313278" TEXT="Energ&#xed;a mec&#xe1;nica" VSHIFT="-2">
+<node CREATED="1440308499874" ID="ID_1576483660" MODIFIED="1440309191514" TEXT="a trav&#xe9;s de">
+<icon BUILTIN="idea"/>
+<node CREATED="1440308504419" ID="ID_278577093" MODIFIED="1440308513155" TEXT="Motores">
+<node CREATED="1440308513156" ID="ID_382386012" MODIFIED="1440310187832" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1440308519267" ID="ID_1710586608" MODIFIED="1440308548805" TEXT="Motores t&#xe9;rmicos">
+<node CREATED="1440308548806" ID="ID_383150789" MODIFIED="1440308692139" TEXT="incluyendo">
+<icon BUILTIN="idea"/>
+<node CREATED="1440308572245" ID="ID_1660931022" MODIFIED="1440308686394">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -M&#225;quina de vapor
+    </p>
+    <p>
+      -Motor Otto de cuatro tiempos
+    </p>
+    <p>
+      -Motor diesel
+    </p>
+    <p>
+      -Motor Otto de dos tiempos
+    </p>
+    <p>
+      -Turbina de gas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1440308563710" ID="ID_1450306489" MODIFIED="1440308570311" TEXT="Motores el&#xe9;ctricos"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1440308860070" HGAP="33" ID="ID_1287880935" MODIFIED="1440309293321" TEXT="Energ&#xed;a ac&#xfa;stica" VSHIFT="23">
+<node CREATED="1440308873976" ID="ID_1727399646" MODIFIED="1440309188955" TEXT="a trav&#xe9;s de">
+<icon BUILTIN="idea"/>
+<node CREATED="1440308950034" ID="ID_1688737848" MODIFIED="1440308982285" TEXT="Instrumentos musicales">
+<node CREATED="1440308982286" ID="ID_499692960" MODIFIED="1440309744106" TEXT="producen">
+<icon BUILTIN="idea"/>
+<node CREATED="1440308989397" ID="ID_1284527112" MODIFIED="1440308995117" TEXT="Ondas viajeras">
+<node CREATED="1440308995118" ID="ID_846139725" MODIFIED="1440309073855" TEXT="por ejemplo">
+<icon BUILTIN="idea"/>
+<node CREATED="1440309001843" ID="ID_291079973" MODIFIED="1440309025908" TEXT="Instrumentos de viento"/>
+</node>
+</node>
+<node CREATED="1440309030623" ID="ID_1769801891" MODIFIED="1440309042734" TEXT="Ondas estacionarias">
+<node CREATED="1440309042735" ID="ID_705400262" MODIFIED="1440309076942" TEXT="por ejemplo">
+<icon BUILTIN="idea"/>
+<node CREATED="1440309047223" ID="ID_1444079168" MODIFIED="1440309101781">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Instrumentos de cuerda
+    </p>
+    <p>
+      -Instrumentos de percusi&#243;n
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1440309125229" HGAP="27" ID="ID_1827040328" MODIFIED="1440309287325" TEXT="Energ&#xed;a lum&#xed;nica" VSHIFT="57">
+<node CREATED="1440309133789" ID="ID_52829490" MODIFIED="1440309162484" TEXT="a trav&#xe9;s de">
+<icon BUILTIN="idea"/>
+<node CREATED="1440309137745" ID="ID_1641150211" MODIFIED="1440309142638" TEXT="Bombillos">
+<node CREATED="1440309142639" ID="ID_1611200446" MODIFIED="1440309749600" TEXT="pueden ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1440309149272" ID="ID_1305330184" MODIFIED="1440309185004">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Incandescentes
+    </p>
+    <p>
+      -Fluorescentes
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1440309193829" HGAP="27" ID="ID_1363330908" MODIFIED="1440309283969" TEXT="Energ&#xed;a t&#xe9;rmica" VSHIFT="59">
+<node CREATED="1440309213065" ID="ID_524160090" MODIFIED="1440309217717" TEXT="a trav&#xe9;s de">
+<icon BUILTIN="idea"/>
+<node CREATED="1440309218839" ID="ID_497655084" MODIFIED="1440309227566" TEXT="Calentadores">
+<node CREATED="1440309227567" ID="ID_1428058053" MODIFIED="1440309755248" TEXT="pueden ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1440309233361" ID="ID_1466009401" MODIFIED="1440309261974">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -De combusti&#243;n
+    </p>
+    <p>
+      -El&#233;ctricos
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
