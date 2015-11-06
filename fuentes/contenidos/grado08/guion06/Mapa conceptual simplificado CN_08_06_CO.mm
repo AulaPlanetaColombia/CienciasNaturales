9f722da8790812d04785133f60977f3424d0ec68
Maaljurem <miguelaljure@hotmail.com>
2015-11-05 20:45:46 -0500

    Mapa más simplificado

--- fuentes/contenidos/grado08/guion06/Mapa conceptual simplificado CN_08_06_CO.mm
+++ fuentes/contenidos/grado08/guion06/Mapa conceptual simplificado CN_08_06_CO.mm
@@ -1,7 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1426429769846" ID="ID_1849005272" MODIFIED="1435261555870" TEXT="Los genes">
-<node CREATED="1426431052059" HGAP="35" ID="ID_1431677298" MODIFIED="1438800833644" POSITION="left" TEXT="se localizan en los" VSHIFT="-76">
+<node CREATED="1426431052059" HGAP="38" ID="ID_1431677298" MODIFIED="1446773527135" POSITION="left" TEXT="se localizan en los" VSHIFT="-16">
 <icon BUILTIN="idea"/>
 <node CREATED="1426431069305" HGAP="33" ID="ID_1073839482" MODIFIED="1438800841207" TEXT="Cromosomas" VSHIFT="-11">
 <node CREATED="1426431577980" ID="ID_1789585204" MODIFIED="1426431588442" TEXT="pueden presentar">
@@ -54,8 +54,7 @@
       -Inserciones
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -78,14 +77,19 @@
 <linktarget COLOR="#b0b0b0" DESTINATION="ID_1778280223" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1003574845" SOURCE="ID_1778280223" STARTARROW="None" STARTINCLINATION="0;0;"/>
 <linktarget COLOR="#b0b0b0" DESTINATION="ID_1778280223" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1401870403" SOURCE="ID_1778280223" STARTARROW="None" STARTINCLINATION="0;0;"/>
 <icon BUILTIN="idea"/>
-<node CREATED="1426430103676" HGAP="27" ID="ID_474201780" MODIFIED="1438800802664" TEXT="Fenotipo" VSHIFT="-2"/>
+<node CREATED="1426430103676" HGAP="27" ID="ID_474201780" MODIFIED="1438800802664" TEXT="Fenotipo" VSHIFT="-2">
+<node CREATED="1436125921892" ID="ID_605628085" MODIFIED="1446772686346" TEXT="al indicar">
+<icon BUILTIN="idea"/>
+<node CREATED="1436125932687" ID="ID_1993908856" MODIFIED="1436125953420" TEXT="Qu&#xe9; prote&#xed;nas se sintetizan"/>
+</node>
+</node>
 </node>
 </node>
 </node>
 <node CREATED="1426430762428" HGAP="18" ID="ID_1115008483" MODIFIED="1435261806701" TEXT="se presentan en" VSHIFT="36">
 <icon BUILTIN="idea"/>
 <node CREATED="1426430788173" HGAP="21" ID="ID_739231426" MODIFIED="1435261890917" TEXT="Alelos" VSHIFT="7">
-<node CREATED="1426430793745" HGAP="31" ID="ID_680794931" MODIFIED="1436980353376" TEXT="pueden ser" VSHIFT="13">
+<node CREATED="1426430793745" HGAP="32" ID="ID_680794931" MODIFIED="1446774099470" TEXT="pueden ser" VSHIFT="25">
 <icon BUILTIN="idea"/>
 <node CREATED="1426430801443" HGAP="12" ID="ID_838377594" MODIFIED="1435261896804" VSHIFT="-14">
 <richcontent TYPE="NODE"><html>
@@ -104,38 +108,32 @@
     </p>
   </body>
 </html></richcontent>
-<node CREATED="1426431427183" HGAP="58" ID="ID_1341601087" MODIFIED="1436125814345" TEXT="se transmiten" VSHIFT="-31">
+</node>
+</node>
+<node CREATED="1426431427183" HGAP="31" ID="ID_1341601087" MODIFIED="1446774101764" TEXT="se transmiten" VSHIFT="35">
 <icon BUILTIN="idea"/>
-<node CREATED="1426431473813" HGAP="22" ID="ID_886773322" MODIFIED="1435261384851" TEXT="A trav&#xe9;s de las generaciones" VSHIFT="-35">
-<node CREATED="1426431511450" HGAP="17" ID="ID_1533922984" MODIFIED="1435261419677" TEXT="a trav&#xe9;s de" VSHIFT="-49">
-<icon BUILTIN="idea"/>
-<node CREATED="1426430801443" ID="ID_1413150151" MODIFIED="1438800775972">
+<node CREATED="1426431473813" HGAP="22" ID="ID_886773322" MODIFIED="1446774042434" VSHIFT="-35">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      -Mitosis
-    </p>
-    <p>
-      -Meiosis
+      A trav&#233;s de la divisi&#243;n celular (mitosis y meiosis)
     </p>
   </body>
 </html>
 </richcontent>
 </node>
-</node>
-</node>
-<node CREATED="1426429914929" HGAP="18" ID="ID_626052686" MODIFIED="1435261875105" TEXT="Seg&#xfa;n las leyes de la herencia de Mendel" VSHIFT="34">
-<node CREATED="1429475892184" HGAP="13" ID="ID_402626123" MODIFIED="1436980379229" TEXT="son" VSHIFT="31">
-<icon BUILTIN="idea"/>
-<node CREATED="1429475944692" ID="ID_327780515" MODIFIED="1435257893387">
+<node CREATED="1426429914929" HGAP="18" ID="ID_626052686" MODIFIED="1446774072771" VSHIFT="34">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
+    <p>
+      Seg&#250;n las leyes de la herencia de Mendel:
+    </p>
     <p>
       -Ley de la uniformidad
     </p>
@@ -146,18 +144,11 @@
       -Ley de la independencia de caracteres
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
 </node>
 </node>
 </node>
-</node>
-</node>
-</node>
-</node>
-</node>
-<node CREATED="1436125921892" ID="ID_605628085" MODIFIED="1436980364063" TEXT="indican">
-<icon BUILTIN="idea"/>
-<node CREATED="1436125932687" ID="ID_1993908856" MODIFIED="1436125953420" TEXT="Qu&#xe9; prote&#xed;nas se sintetizan"/>
 </node>
 </node>
 </node>
