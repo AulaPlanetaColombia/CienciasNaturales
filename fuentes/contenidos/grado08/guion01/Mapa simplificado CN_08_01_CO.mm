f42dcaf1d341b785e91feaf95ceb14462b5bed10
Maaljurem <miguelaljure@hotmail.com>
2015-11-22 21:30:03 -0500

    Corrección escaleta numeración recursos CN_08_01_CO
    Se cambia el f13 por f6

---
+++ fuentes/contenidos/grado08/guion01/Mapa simplificado CN_08_01_CO.mm
@@ -0,0 +1,152 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1436707510039" ID="ID_351602393" MODIFIED="1436707599079" TEXT="El sistema nervioso">
+<node CREATED="1436708060090" ID="ID_1606485572" MODIFIED="1436736100581" POSITION="right" TEXT="se divide en">
+<icon BUILTIN="idea"/>
+<node CREATED="1436708072373" ID="ID_836604877" MODIFIED="1436736021192" TEXT="Sistema nervioso central">
+<node CREATED="1436708112727" HGAP="29" ID="ID_1546694924" MODIFIED="1443544681040" TEXT="integrado por" VSHIFT="-2">
+<icon BUILTIN="idea"/>
+<node CREATED="1436708424465" ID="ID_524976808" MODIFIED="1443544713530">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -M&#233;dula espinal
+    </p>
+    <p>
+      -Enc&#233;falo (cerebro, cerebelo y tronco encef&#225;lico)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1436708085594" HGAP="28" ID="ID_1120730401" MODIFIED="1436751626132" TEXT="Sistema nervioso perif&#xe9;rico" VSHIFT="33">
+<node CREATED="1436708439833" ID="ID_1320310316" MODIFIED="1436708447516" TEXT="integrado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1436708449880" ID="ID_1467110206" MODIFIED="1443544597932">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Nervios sensoriales
+    </p>
+    <p>
+      - Nervios motores
+    </p>
+    <p>
+      - Ganglios nerviosos
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1436751559466" ID="ID_1540198808" MODIFIED="1436751564016" TEXT="se divide en">
+<icon BUILTIN="idea"/>
+<node CREATED="1436751565130" ID="ID_279069119" MODIFIED="1436751601333" TEXT="Sistema nervioso som&#xe1;tico"/>
+<node CREATED="1436751579041" HGAP="17" ID="ID_1992298389" MODIFIED="1436751895888" TEXT="Sistema nervioso aut&#xf3;nomo" VSHIFT="24">
+<node CREATED="1436751620208" ID="ID_557718908" MODIFIED="1436751885471" TEXT="se divide en">
+<arrowlink DESTINATION="ID_557718908" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_994072147" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_557718908" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_994072147" SOURCE="ID_557718908" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1436751809795" ID="ID_305460847" MODIFIED="1443544657230">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Sistema simp&#225;tico
+    </p>
+    <p>
+      -Sistema parasimp&#225;tico
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1436754100011" ID="ID_161530218" MODIFIED="1436754104557" POSITION="left" TEXT="se compone de">
+<icon BUILTIN="idea"/>
+<node CREATED="1436754105831" HGAP="23" ID="ID_1426090987" MODIFIED="1436754373470" TEXT="Neuronas" VSHIFT="-29">
+<node CREATED="1436754115343" HGAP="22" ID="ID_1219687111" MODIFIED="1436754388773" TEXT="pueden ser" VSHIFT="-37">
+<icon BUILTIN="idea"/>
+<node CREATED="1436754119381" ID="ID_241293359" MODIFIED="1436754148118">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Sensitivas
+    </p>
+    <p>
+      - Motoras
+    </p>
+    <p>
+      - Interneuronas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1436754231078" ID="ID_1391993604" MODIFIED="1436754349429" TEXT="se componen de">
+<icon BUILTIN="idea"/>
+<node CREATED="1436754342698" ID="ID_412791631" MODIFIED="1436754366785">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Ax&#243;n
+    </p>
+    <p>
+      - Dendritas
+    </p>
+    <p>
+      - Cuerpo neuronal
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1436754840451" HGAP="27" ID="ID_1717202908" MODIFIED="1443544549533" TEXT="transmiten" VSHIFT="23">
+<icon BUILTIN="idea"/>
+<node CREATED="1436754873422" ID="ID_673426167" MODIFIED="1443544545877">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Se&#241;ales el&#233;ctricas
+    </p>
+    <p>
+      -Se&#241;ales qu&#237;micas
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1436754176881" ID="ID_568494720" MODIFIED="1436754183542" TEXT="C&#xe9;lulas gliales">
+<node CREATED="1436754183544" ID="ID_1661933693" MODIFIED="1443544444320" TEXT="encargadas de">
+<icon BUILTIN="idea"/>
+<node CREATED="1436754189188" ID="ID_584639478" MODIFIED="1436754208551" TEXT="Proteger a las neuronas y aislarlas el&#xe9;ctricamente"/>
+</node>
+</node>
+</node>
+</node>
+</map>
