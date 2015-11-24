c8f3c1c3824db0ff86e649ecdc2bb7d026f1f397
diegomol88 <diegomol16@gmail.com>
2015-11-24 09:23:23 -0500

    actualización mapa CN 06 05
    actualización mapa CN 06 05

---
+++ fuentes/contenidos/grado06/guion05/CN_06_05_REC280.mm
@@ -0,0 +1,183 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1448370052708" ID="ID_1709648462" MODIFIED="1448372300786" TEXT="La nutrici&#xf3;n y la digesti&#xf3;n en el ser humano">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1448372335166" ID="ID_1486486761" MODIFIED="1448373651095" POSITION="right" TEXT="involucra los sistemas">
+<icon BUILTIN="idea"/>
+<node CREATED="1448372459401" ID="ID_1547146889" MODIFIED="1448373662578">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Circulatorio
+    </p>
+    <p>
+      - Respiratorio
+    </p>
+    <p>
+      - Excretor
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1448373666661" ID="ID_585107667" MODIFIED="1448373950558" TEXT="- Digestivo">
+<node CREATED="1448373865805" ID="ID_248640237" MODIFIED="1448373935421" TEXT=" que tiene como funci&#xf3;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1448373884088" ID="ID_145823092" MODIFIED="1448373931452" TEXT="- la ingesti&#xf3;n, transformaci&#xf3;n y absorci&#xf3;n de alimentos "/>
+</node>
+<node CREATED="1448373953354" ID="ID_1155229610" MODIFIED="1448374841993" TEXT="que est&#xe1; formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1448373971674" ID="ID_1141663593" MODIFIED="1448373983585" TEXT="el tubo digestivo">
+<node CREATED="1448374521273" ID="ID_612546988" MODIFIED="1448374679901" TEXT="el cual contiene">
+<node CREATED="1448374684447" ID="ID_1835863607" MODIFIED="1448374874588">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - la boca
+    </p>
+    <p>
+      - la faringe
+    </p>
+    <p>
+      - el es&#243;fago
+    </p>
+    <p>
+      - el est&#243;mago
+    </p>
+    <p>
+      - el intestino delgado
+    </p>
+    <p>
+      - el intestino grueso
+    </p>
+    <p>
+      - el ano
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1448373985026" ID="ID_789838377" MODIFIED="1448373993282" TEXT="las gl&#xe1;ndulas anexas">
+<node CREATED="1448374521273" ID="ID_291950036" MODIFIED="1448374744502" TEXT="que son">
+<node CREATED="1448374684447" ID="ID_869760573" MODIFIED="1448374765550">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - las gl&#225;ndulas salivales
+    </p>
+    <p>
+      - el h&#237;gado
+    </p>
+    <p>
+      - el p&#225;ncreas
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1448372622047" ID="ID_1326583254" MODIFIED="1448372901850" POSITION="left" TEXT="requiere de">
+<icon BUILTIN="idea"/>
+<node CREATED="1448372629892" ID="ID_1860113429" MODIFIED="1448372691564" TEXT="Alimentos">
+<node CREATED="1448372881736" ID="ID_751487883" MODIFIED="1448372899659" TEXT="para ">
+<icon BUILTIN="idea"/>
+<node CREATED="1448372892147" ID="ID_1753370286" MODIFIED="1448373010384" TEXT="la obtenci&#xf3;n de nutrientes y energ&#xed;a"/>
+</node>
+<node CREATED="1448372920255" ID="ID_1285157503" MODIFIED="1448372984865" TEXT="los cuales se clasifican">
+<icon BUILTIN="idea"/>
+<node CREATED="1448372936364" ID="ID_1226710367" MODIFIED="1448373024853">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - seg&#250;n su procedencia
+    </p>
+    <p>
+      - seg&#250;n su funci&#243;n
+    </p>
+    <p>
+      - seg&#250;n su composici&#243;n qu&#237;mica
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1448373049309" ID="ID_1741786081" MODIFIED="1448373126830" TEXT="Una alimentaci&#xf3;n sana">
+<node CREATED="1448373058126" ID="ID_698895811" MODIFIED="1448373133231" TEXT="a partir de">
+<icon BUILTIN="idea"/>
+<node CREATED="1448373065206" ID="ID_1813642759" MODIFIED="1448373138119" TEXT="una dieta equilibrada"/>
+</node>
+</node>
+</node>
+<node CREATED="1448374133213" ID="ID_139313055" MODIFIED="1448374360120" POSITION="right" TEXT="se puede ver afectada por">
+<icon BUILTIN="idea"/>
+<node CREATED="1448374150989" ID="ID_1013028030" MODIFIED="1448374158808" TEXT="Enfermedades ">
+<node CREATED="1448374184922" ID="ID_660031951" MODIFIED="1448374193368" TEXT="como por ejemplo">
+<icon BUILTIN="idea"/>
+<node CREATED="1448374196649" ID="ID_163016596" MODIFIED="1448374239880">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Caries
+    </p>
+    <p>
+      - Gastritis
+    </p>
+    <p>
+      - Colon Irritable
+    </p>
+    <p>
+      - Diarrea
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1448374287189" ID="ID_357280532" MODIFIED="1448374297956" TEXT="Trastornos alimenticios">
+<node CREATED="1448374184922" ID="ID_1588216767" MODIFIED="1448374193368" TEXT="como por ejemplo">
+<icon BUILTIN="idea"/>
+<node CREATED="1448374196649" ID="ID_1762634903" MODIFIED="1448374336692">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - la anorexia
+    </p>
+    <p>
+      - la bulimia
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
