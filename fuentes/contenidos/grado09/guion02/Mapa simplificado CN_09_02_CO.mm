c0a6028785d8204014ba71f9124096be3b329dd6
mcabiativa <mcabiativa@gmail.com>
2015-12-30 17:29:26 -0500

    Merge branche
    Restauracion GitHub mi sesión

---
+++ fuentes/contenidos/grado09/guion02/Mapa simplificado CN_09_02_CO.mm
@@ -0,0 +1,115 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1436746759287" ID="ID_1475305433" MODIFIED="1436746852754" TEXT="La gen&#xe9;tica molecular moderna">
+<node CREATED="1436747303558" HGAP="43" ID="ID_1480390641" MODIFIED="1438455608622" POSITION="right" TEXT="se estudia con" VSHIFT="-8">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747317968" HGAP="18" ID="ID_736965858" MODIFIED="1438455627389" TEXT="Grandes proyectos de investigaci&#xf3;n colaborativos" VSHIFT="-15">
+<arrowlink DESTINATION="ID_736965858" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_653600568" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_736965858" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_653600568" SOURCE="ID_736965858" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1438455554818" ID="ID_70665646" MODIFIED="1438455560340" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747644828" ID="ID_692053305" MODIFIED="1438455624519">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -El Proyecto Genoma Humano
+    </p>
+    <p>
+      -El proyecto ENCODE
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1438455573873" ID="ID_343153114" MODIFIED="1438455632771" TEXT="El apoyo de otras disciplinas cient&#xed;ficas" VSHIFT="11">
+<node CREATED="1438455584185" ID="ID_1956244083" MODIFIED="1438455586619" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1438455587851" ID="ID_883744058" MODIFIED="1438455595199" TEXT="La bioinform&#xe1;tica"/>
+</node>
+</node>
+</node>
+<node CREATED="1438455272483" ID="ID_1594987148" MODIFIED="1438455277724" POSITION="left" TEXT="permite">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747715915" ID="ID_434849552" MODIFIED="1436747767348" TEXT="El uso de seres vivos para generar tecnolog&#xed;a">
+<node CREATED="1436747773907" ID="ID_1030240997" MODIFIED="1438455384684" TEXT="usada en">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747785719" ID="ID_679457208" MODIFIED="1438455409893">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -La medicina
+    </p>
+    <p>
+      -Estudio y conservaci&#243;n del medio ambiente
+    </p>
+    <p>
+      -La agricultura
+    </p>
+    <p>
+      -La ganader&#237;a
+    </p>
+    <p>
+      -La investigaci&#243;n cient&#237;fica b&#225;sica
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1438454937121" ID="ID_1304529101" MODIFIED="1438455360099" TEXT="a trav&#xe9;s de" VSHIFT="50">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747315738" ID="ID_311051809" MODIFIED="1436747333941" TEXT="La manipulaci&#xf3;n gen&#xe9;tica">
+<node CREATED="1436747346542" ID="ID_1067002088" MODIFIED="1438454911459" TEXT="genera">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747386754" ID="ID_132469990" MODIFIED="1438454813503" TEXT="Organismos gen&#xe9;ticamente modificados (OGM)">
+<node CREATED="1438454816919" ID="ID_1051814610" MODIFIED="1438454822270" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747395729" ID="ID_1125049916" MODIFIED="1438454922426" TEXT="Los organismos transg&#xe9;nicos"/>
+</node>
+</node>
+</node>
+<node CREATED="1436747430454" HGAP="27" ID="ID_1534124110" MODIFIED="1438455356387" TEXT="usando" VSHIFT="31">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747441562" ID="ID_1145968463" MODIFIED="1438454874921" TEXT="T&#xe9;cnicas de ingenier&#xed;a gen&#xe9;tica">
+<node CREATED="1436747463646" ID="ID_475168927" MODIFIED="1436747468314" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1436747471586" ID="ID_1445196126" MODIFIED="1438454887588">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      -Extracci&#243;n de ADN
+    </p>
+    <p>
+      -PCR
+    </p>
+    <p>
+      -Uso de enzimas de restricci&#243;n
+    </p>
+    <p>
+      -Electrofor&#233;sis
+    </p>
+    <p>
+      -Secuenciaci&#243;n
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
