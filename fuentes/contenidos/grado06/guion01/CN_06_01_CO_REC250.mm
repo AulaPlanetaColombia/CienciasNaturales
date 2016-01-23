d914836d31fe90f8f35fc613f1c2b3638448e216
diegomol88 <diegomol16@gmail.com>
2016-01-23 18:46:30 -0500

    Guion CN 06 01, actualización mapa y escaleta
    Guion CN 06 01, actualización mapa y escaleta

---
+++ fuentes/contenidos/grado06/guion01/CN_06_01_CO_REC250.mm
@@ -0,0 +1,85 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453508397019" ID="ID_410670971" MODIFIED="1453508411115" TEXT="El origen del universo y de la vida">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1453508414479" ID="ID_1084839025" MODIFIED="1453508880589" POSITION="right" TEXT="se explica mediante ">
+<icon BUILTIN="idea"/>
+<node CREATED="1453508510313" ID="ID_1333252332" MODIFIED="1453508520703" TEXT="Visiones no cient&#xed;ficas">
+<node CREATED="1442112408126" ID="ID_860083829" MODIFIED="1453508892007" TEXT="de car&#xe1;cter">
+<icon BUILTIN="idea"/>
+<node CREATED="1442112419345" ID="ID_165214918" MODIFIED="1453491180425" TEXT="m&#xed;tico"/>
+<node CREATED="1453491183078" ID="ID_1916910080" MODIFIED="1453491186994" TEXT="religioso">
+<node CREATED="1453491188403" ID="ID_1244856538" MODIFIED="1453491193601" TEXT="como ">
+<icon BUILTIN="idea"/>
+<node CREATED="1453491196049" ID="ID_1770699413" MODIFIED="1453491201016" TEXT="el creacionismo"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1453508528749" ID="ID_1795286526" MODIFIED="1453508535854" TEXT="Teor&#xed;as cient&#xed;ficas">
+<node CREATED="1453508797709" ID="ID_1582677993" MODIFIED="1453510144403" TEXT="que se refieren">
+<icon BUILTIN="idea"/>
+<node CREATED="1453508814437" ID="ID_626341781" MODIFIED="1453510170166" TEXT="al comienzo de la vida en la Tierra">
+<node CREATED="1432740725416" ID="ID_322051323" MODIFIED="1453509130126" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1432740786885" ID="ID_556230781" MODIFIED="1442111771823" TEXT="la teor&#xed;a quimiosint&#xe9;tica"/>
+<node CREATED="1432740832810" ID="ID_1797942152" MODIFIED="1442111777379" TEXT="la panspermia"/>
+</node>
+<node CREATED="1453509633650" ID="ID_549231302" MODIFIED="1453510202730" TEXT="y que permiten">
+<icon BUILTIN="idea"/>
+<node CREATED="1453509870289" ID="ID_1674920558" MODIFIED="1453510209254" TEXT="entender las caracter&#xed;sticas de todo ser vivo"/>
+</node>
+</node>
+<node CREATED="1453508824172" ID="ID_854975820" MODIFIED="1453510156313" TEXT="a la  formaci&#xf3;n del universo">
+<node CREATED="1442110926183" ID="ID_376278720" MODIFIED="1453509204751" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1432740786885" ID="ID_1765309372" MODIFIED="1453505468017">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - el Big Bang
+    </p>
+    <p>
+      - el universo oscilante
+    </p>
+    <p>
+      - el universo estacionario
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1432741040424" ID="ID_1018053501" MODIFIED="1453510229462">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      a la formaci&#243;n de:
+    </p>
+    <p>
+      - las galaxias
+    </p>
+    <p>
+      - las estrellas
+    </p>
+    <p>
+      - el sistema solar
+    </p>
+    <p>
+      - la Tierra
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
