88c583d4cd215af7803d22d65db35a4162c86339
diegomol88 <diegomol16@gmail.com>
2016-01-23 18:47:05 -0500

    Mapa CN 06 06
    Mapa CN 06 06

---
+++ fuentes/contenidos/grado06/guion06/CN_06_06_CO_REC270.mm
@@ -0,0 +1,116 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453209120088" ID="ID_256968119" MODIFIED="1453217848842" TEXT="La respiraci&#xf3;n en los seres vivos">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1453209565863" ID="ID_1447408829" MODIFIED="1453209657559" POSITION="right" TEXT="les permite">
+<icon BUILTIN="idea"/>
+<node CREATED="1453209580002" ID="ID_1793447466" MODIFIED="1453212254093" TEXT="La obtenci&#xf3;n de energ&#xed;a">
+<node CREATED="1453212217017" ID="ID_1354458268" MODIFIED="1453212247832" TEXT="para">
+<icon BUILTIN="idea"/>
+<node CREATED="1453212223737" ID="ID_1920566922" MODIFIED="1453212231900" TEXT="el desarrollo de las funciones vitales"/>
+</node>
+</node>
+</node>
+<node CREATED="1453212280324" ID="ID_18757188" MODIFIED="1453212286927" POSITION="left" TEXT="puede ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1453212288155" ID="ID_1927724061" MODIFIED="1453212293709" TEXT="Aer&#xf3;bica">
+<node CREATED="1453212635787" ID="ID_455195307" MODIFIED="1453213090960" TEXT="la cual ocurre ">
+<icon BUILTIN="idea"/>
+<node CREATED="1453212827744" ID="ID_617828113" MODIFIED="1453213100913" TEXT="en presencia de ox&#xed;geno"/>
+</node>
+</node>
+<node CREATED="1453212298393" ID="ID_1855090216" MODIFIED="1453212303173" TEXT="Anaer&#xf3;bica">
+<node CREATED="1453212796841" ID="ID_1983167855" MODIFIED="1453213094278" TEXT="la cual ocurre">
+<icon BUILTIN="idea"/>
+<node CREATED="1453212844956" ID="ID_266751358" MODIFIED="1453213097410" TEXT="en ausencia de ox&#xed;geno "/>
+</node>
+</node>
+</node>
+<node CREATED="1453221851539" ID="ID_1909615257" MODIFIED="1453229462868" POSITION="right" TEXT="se entiende como">
+<icon BUILTIN="idea"/>
+<node CREATED="1453221878675" ID="ID_209679816" MODIFIED="1453229582063" TEXT="El intercambio de gases">
+<node CREATED="1453226299965" ID="ID_1739122193" MODIFIED="1453226314832" TEXT="entre">
+<icon BUILTIN="idea"/>
+<node CREATED="1453226303820" ID="ID_231097029" MODIFIED="1453226311100" TEXT="un organismo y el medio"/>
+</node>
+</node>
+<node CREATED="1453229464637" ID="ID_303803423" MODIFIED="1453229584734" TEXT="El conjunto de reacciones qu&#xed;micas">
+<node CREATED="1453229514452" ID="ID_531880515" MODIFIED="1453238893244" TEXT="que permite">
+<icon BUILTIN="idea"/>
+<node CREATED="1453229518440" ID="ID_148546906" MODIFIED="1453229524676" TEXT="degradar nutrientes en la c&#xe9;lula"/>
+</node>
+</node>
+</node>
+<node CREATED="1453226580233" ID="ID_1517597183" MODIFIED="1453237899020" POSITION="left" TEXT="se realiza de forma">
+<node CREATED="1453237286379" ID="ID_1151623483" MODIFIED="1453237290576" TEXT="Directa">
+<node CREATED="1453237306106" ID="ID_548816416" MODIFIED="1453238575978" TEXT="cuando es">
+<icon BUILTIN="idea"/>
+<node CREATED="1453237333552" ID="ID_1919757311" MODIFIED="1453239834226" TEXT="entre las c&#xe9;lulas y el medio externo">
+<node CREATED="1453237385145" ID="ID_557695602" MODIFIED="1453238588304" TEXT="tal como lo hacen">
+<icon BUILTIN="idea"/>
+<node CREATED="1453237413954" ID="ID_406298619" MODIFIED="1453237420286" TEXT="microorganismos y hongos"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1453237293154" ID="ID_1409516510" MODIFIED="1453237296407" TEXT="Indirecta">
+<node CREATED="1453237445453" ID="ID_1342086645" MODIFIED="1453238605683" TEXT="cuando es">
+<icon BUILTIN="idea"/>
+<node CREATED="1453237456570" ID="ID_49116098" MODIFIED="1453238929826" TEXT="por medio de estructuras especializadas">
+<node CREATED="1453238617940" ID="ID_49447937" MODIFIED="1453238625201" TEXT="tal como lo hacen     ">
+<node CREATED="1453238627882" ID="ID_1007294150" MODIFIED="1453238642195" TEXT="las plantas">
+<node CREATED="1453238644721" ID="ID_537071716" MODIFIED="1453238647694" TEXT="a trav&#xe9;s de">
+<node CREATED="1453238649284" ID="ID_1909367420" MODIFIED="1453238691794">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - estomas
+    </p>
+    <p>
+      - lenticelas
+    </p>
+    <p>
+      - neumat&#243;foros
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1453238695347" ID="ID_925858752" MODIFIED="1453238697991" TEXT="los animales">
+<node CREATED="1453238817112" ID="ID_1162988564" MODIFIED="1453238820028" TEXT="a trav&#xe9;s de">
+<node CREATED="1453238823418" ID="ID_472595263" MODIFIED="1453238951248">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - tr&#225;queas
+    </p>
+    <p>
+      - branquias
+    </p>
+    <p>
+      - la piel
+    </p>
+    <p>
+      - pulmones
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
