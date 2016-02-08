7855900f7d893349b70a7b551c07cb0e4a756c35
diegomol88 <diegomol16@gmail.com>
2016-02-08 08:44:59 -0500

    Mapa CN 07 05
    Mapa CN 07 05

---
+++ fuentes/contenidos/grado07/guion05/CN_07_05_REC240.mm
@@ -0,0 +1,138 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1454884206088" ID="ID_361138751" MODIFIED="1454938824538" TEXT="El metabolismo, la homeostasis y la excreci&#xf3;n en los seres vivos">
+<node CREATED="1454888813481" ID="ID_1052225373" MODIFIED="1454938855460" POSITION="right" TEXT="implica">
+<icon BUILTIN="idea"/>
+<node CREATED="1454888816810" ID="ID_1469429568" MODIFIED="1454889307113" TEXT="Las reacciones bioqu&#xed;micas a nivel celular (metabolismo)">
+<node CREATED="1454888910434" ID="ID_1113675232" MODIFIED="1454889269107" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1454888915941" ID="ID_925595152" MODIFIED="1454889042412" TEXT="catab&#xf3;licas o de degradaci&#xf3;n">
+<node CREATED="1454889231151" ID="ID_179069994" MODIFIED="1454889271404" TEXT="con liberaci&#xf3;n de">
+<icon BUILTIN="idea"/>
+<node CREATED="1454889258384" ID="ID_1239248398" MODIFIED="1454889260692" TEXT="energ&#xed;a"/>
+</node>
+</node>
+<node CREATED="1454888931099" ID="ID_1527071363" MODIFIED="1454889054027" TEXT="anab&#xf3;licas o de s&#xed;ntesis">
+<node CREATED="1454889251313" ID="ID_203270023" MODIFIED="1454889274443" TEXT="con consumo de">
+<icon BUILTIN="idea"/>
+<node CREATED="1454889264235" ID="ID_1343062744" MODIFIED="1454889266937" TEXT="energ&#xed;a"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1454888836062" ID="ID_970384157" MODIFIED="1454889313744" TEXT="La regulaci&#xf3;n interna del organismo (home&#xf3;stasis)">
+<node CREATED="1454890809283" ID="ID_951311115" MODIFIED="1454895834159" TEXT="como por ejemplo de">
+<icon BUILTIN="idea"/>
+<node CREATED="1454890818218" ID="ID_1307295234" MODIFIED="1454895849258">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - la temperatura
+    </p>
+    <p>
+      - la concentraci&#243;n de iones
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1454888846495" ID="ID_1514861171" MODIFIED="1454888867357" TEXT="La excreci&#xf3;n de desechos">
+<node CREATED="1454891575548" ID="ID_957408528" MODIFIED="1454895944236" TEXT="en">
+<icon BUILTIN="idea"/>
+<node CREATED="1454891586444" ID="ID_82322051" MODIFIED="1454891602336" TEXT="microorganismos">
+<node CREATED="1454891702647" ID="ID_1492089992" MODIFIED="1454896282979" TEXT="y se realiza por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1454891721086" ID="ID_365459372" MODIFIED="1454891768712" TEXT="mecanismos de transporte celular y vacuolas"/>
+</node>
+</node>
+<node CREATED="1454891588717" ID="ID_1620914126" MODIFIED="1454895965305" TEXT="plantas">
+<node CREATED="1454895966531" ID="ID_1924963861" MODIFIED="1454896287795" TEXT="y se realiza por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1454891787268" ID="ID_500949175" MODIFIED="1454891816797">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - estomas
+    </p>
+    <p>
+      - lenticelas
+    </p>
+    <p>
+      - hid&#225;todos
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1454891590792" ID="ID_1202230892" MODIFIED="1454891613104" TEXT="animales">
+<node CREATED="1454896325850" ID="ID_1076473632" MODIFIED="1454896328697" TEXT="como los">
+<node CREATED="1454896330981" ID="ID_791115550" MODIFIED="1454896335564" TEXT="invertebrados">
+<node CREATED="1454896446538" ID="ID_1489753632" MODIFIED="1454896590921" TEXT="y se realiza por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1454896465403" ID="ID_1885029584" MODIFIED="1454896509470">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - difusi&#243;n
+    </p>
+    <p>
+      - protonefridios
+    </p>
+    <p>
+      - metanefridios
+    </p>
+    <p>
+      - tubos de malpigui
+    </p>
+    <p>
+      - gl&#225;ndulas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1454896337269" ID="ID_108300180" MODIFIED="1454896419175" TEXT="vertebrados">
+<node CREATED="1454896532817" ID="ID_813440052" MODIFIED="1454896593058" TEXT="y se realiza por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1454896539365" ID="ID_525518350" MODIFIED="1454896570413">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - la piel
+    </p>
+    <p>
+      - los pulmones
+    </p>
+    <p>
+      - el h&#237;gado
+    </p>
+    <p>
+      - el sistema urinario
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
