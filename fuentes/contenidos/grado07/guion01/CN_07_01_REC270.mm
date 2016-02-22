693d88c509d63861e15424707a85887fa343aadc
diegomol88 <diegomol16@gmail.com>
2016-02-22 07:53:48 -0500

    Mapa CN 07 01
    Mapa CN 07 01

---
+++ fuentes/contenidos/grado07/guion01/CN_07_01_REC270.mm
@@ -0,0 +1,405 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1456141426474" ID="ID_1557812996" MODIFIED="1456142641078" TEXT="Los sistemas de locomoci&#xf3;n en los seres vivos">
+<node CREATED="1456141514908" ID="ID_1742894123" MODIFIED="1456141753180" POSITION="right" TEXT="ocurre">
+<icon BUILTIN="idea"/>
+<node CREATED="1456141520198" ID="ID_495683365" MODIFIED="1456141977510" TEXT="A nivel microsc&#xf3;pico ">
+<node CREATED="1456141999202" ID="ID_1839046903" MODIFIED="1456142645450" TEXT="como en ">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142008072" ID="ID_1116704651" MODIFIED="1456142010685" TEXT="bacterias">
+<node CREATED="1456142347206" ID="ID_1172305347" MODIFIED="1456142648265" TEXT="lo realizan por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142368013" ID="ID_691987090" MODIFIED="1456142456330">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - movimientos flagelares
+    </p>
+    <p>
+      - deslizamiento
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1456142425335" ID="ID_386368715" MODIFIED="1456142651074" TEXT="debido a">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142482181" ID="ID_454228944" MODIFIED="1456142487868" TEXT="respuestas sensoriales">
+<node CREATED="1456142489939" ID="ID_607527424" MODIFIED="1456142657521" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142501336" ID="ID_332049782" MODIFIED="1456142533904">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - quimiotaxia
+    </p>
+    <p>
+      - aerotaxia
+    </p>
+    <p>
+      - fototaxia
+    </p>
+    <p>
+      - magnetotaxia
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456142014004" ID="ID_139063870" MODIFIED="1456142021311" TEXT="arqueas">
+<node CREATED="1456142553083" ID="ID_1001464977" MODIFIED="1456142654105" TEXT="lo realizan por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142368013" ID="ID_105285606" MODIFIED="1456142456330">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - movimientos flagelares
+    </p>
+    <p>
+      - deslizamiento
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1456142025023" ID="ID_263493709" MODIFIED="1456142038311" TEXT="protistas">
+<node CREATED="1456142814043" ID="ID_474404299" MODIFIED="1456142940562" TEXT="que pueden ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142920217" ID="ID_45745524" MODIFIED="1456142926012" TEXT="protozoos">
+<node CREATED="1456142553083" ID="ID_1960550004" MODIFIED="1456143307308" TEXT="se mueven debido a ">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142368013" ID="ID_394714691" MODIFIED="1456142789610">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - flagelos
+    </p>
+    <p>
+      - Cilios
+    </p>
+    <p>
+      - pseud&#243;podos
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1456142933406" ID="ID_1312696391" MODIFIED="1456142935194" TEXT="algas">
+<node CREATED="1456143168706" ID="ID_1691261964" MODIFIED="1456143321513" TEXT="con">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143188689" ID="ID_1050707780" MODIFIED="1456143194965" TEXT="mecanismo de locomoci&#xf3;n ">
+<node CREATED="1456143198638" ID="ID_1913781067" MODIFIED="1456143411673" TEXT="por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142368013" ID="ID_262033598" MODIFIED="1456143040607">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - flagelos
+    </p>
+    <p>
+      - deslizamiento
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456143210088" ID="ID_972397830" MODIFIED="1456143324153" TEXT="sin">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143188689" ID="ID_1392486313" MODIFIED="1456143194965" TEXT="mecanismo de locomoci&#xf3;n ">
+<node CREATED="1456143058803" ID="ID_1184960378" MODIFIED="1456143341920" TEXT="se mueven debido a">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143094386" ID="ID_368593653" MODIFIED="1456145394669" TEXT=" las corrientes de agua"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456142666197" ID="ID_1037534455" MODIFIED="1456142668896" TEXT="hongos">
+<node CREATED="1456143168706" ID="ID_680916974" MODIFIED="1456143321513" TEXT="con">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143188689" ID="ID_54424303" MODIFIED="1456143194965" TEXT="mecanismo de locomoci&#xf3;n ">
+<node CREATED="1456143198638" ID="ID_1043963541" MODIFIED="1456143408763" TEXT="por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142368013" ID="ID_1324647901" MODIFIED="1456143386729">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      deslizamiento
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1456143389397" ID="ID_517570302" MODIFIED="1456143421418" TEXT="como lo hacen">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143400069" ID="ID_624351618" MODIFIED="1456143402389" TEXT="las levaduras"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456143210088" ID="ID_583694996" MODIFIED="1456143324153" TEXT="sin">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143188689" ID="ID_713564935" MODIFIED="1456143194965" TEXT="mecanismo de locomoci&#xf3;n ">
+<node CREATED="1456143466996" ID="ID_1047204366" MODIFIED="1456143610505" TEXT="pero">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143498367" ID="ID_1198488453" MODIFIED="1456143639885" TEXT="logran alimentarse y relacionarse">
+<node CREATED="1456143569095" ID="ID_1649146848" MODIFIED="1456143608025" TEXT="debido a">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143574732" ID="ID_1616700648" MODIFIED="1456145387234" TEXT="el crecimiento del micelio "/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456141979281" ID="ID_838354413" MODIFIED="1456141991878" TEXT="A nivel macrosc&#xf3;pico">
+<node CREATED="1456142676818" ID="ID_1336298770" MODIFIED="1456142681849" TEXT="como en">
+<icon BUILTIN="idea"/>
+<node CREATED="1456142683011" ID="ID_1489115131" MODIFIED="1456142687729" TEXT="plantas">
+<node CREATED="1456143728991" ID="ID_989773412" MODIFIED="1456144229337" TEXT="las cuales">
+<icon BUILTIN="idea"/>
+<node CREATED="1456143739643" ID="ID_1224584811" MODIFIED="1456143957218" TEXT="no tienen un sistema de locomoci&#xf3;n propiamente dicho">
+<node CREATED="1456144163435" ID="ID_643037029" MODIFIED="1456144232489" TEXT="pero presentan">
+<icon BUILTIN="idea"/>
+<node CREATED="1456144015752" ID="ID_954410412" MODIFIED="1456144219042">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      peque&#241;os movimientos llamados:
+    </p>
+    <p>
+      - tropismos
+    </p>
+    <p>
+      - nastias
+    </p>
+    <p>
+      - circumnutaciones
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456142689290" ID="ID_778107710" MODIFIED="1456142691960" TEXT="animales">
+<node CREATED="1456144386443" ID="ID_102983875" MODIFIED="1456145203226" TEXT="que poseen">
+<icon BUILTIN="idea"/>
+<node CREATED="1456144414212" ID="ID_1104108285" MODIFIED="1456144789146" TEXT="esqueleto">
+<node CREATED="1456144424409" ID="ID_168218453" MODIFIED="1456145205746" TEXT="puede ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1456144428631" ID="ID_1115136174" MODIFIED="1456144618310">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hidroesqueleto
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1456144564401" ID="ID_795814152" MODIFIED="1456145215353" TEXT="formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1456144569796" ID="ID_1687963831" MODIFIED="1456144610353">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - la cavidad gastrovascular
+    </p>
+    <p>
+      - un fluido
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1456144620877" ID="ID_523193035" MODIFIED="1456144624689" TEXT="exoesqueleto">
+<node CREATED="1456144632397" ID="ID_335468475" MODIFIED="1456145210833" TEXT="formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1456144684685" ID="ID_1694240040" MODIFIED="1456144759459">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - quitina&#160;(insectos)
+    </p>
+    <p>
+      - sales minerales (moluscos)
+    </p>
+    <p>
+      - carbonato de calcio (equinodermos)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1456144800334" ID="ID_929647787" MODIFIED="1456144803617" TEXT="endoesqueleto">
+<node CREATED="1456144849881" ID="ID_1936419646" MODIFIED="1456145216809" TEXT="formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1456144857483" ID="ID_1145323425" MODIFIED="1456144884272">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - hueso
+    </p>
+    <p>
+      - cart&#237;lago
+    </p>
+    <p>
+      - ligamento
+    </p>
+    <p>
+      - articulaci&#243;n
+    </p>
+    <p>
+      - tend&#243;n
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456144921215" ID="ID_444106825" MODIFIED="1456144934188" TEXT="sistema muscular">
+<node CREATED="1456144956274" ID="ID_267995345" MODIFIED="1456145219681" TEXT="formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1456144964294" ID="ID_1541981059" MODIFIED="1456144988953">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - m&#250;sculo estriado
+    </p>
+    <p>
+      - m&#250;sculo liso
+    </p>
+    <p>
+      - m&#250;sculo card&#237;aco
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456145007244" ID="ID_69254838" MODIFIED="1456145225842" TEXT="que presentan">
+<icon BUILTIN="idea"/>
+<node CREATED="1456145060942" ID="ID_270367523" MODIFIED="1456145070090" TEXT="diversas estrategias">
+<node CREATED="1456145077160" ID="ID_1422330835" MODIFIED="1456145222097" TEXT="para">
+<icon BUILTIN="idea"/>
+<node CREATED="1456145080010" ID="ID_1884008708" MODIFIED="1456145112995">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - caminar
+    </p>
+    <p>
+      - correr
+    </p>
+    <p>
+      - saltar
+    </p>
+    <p>
+      - reptar
+    </p>
+    <p>
+      - nadar
+    </p>
+    <p>
+      - volar
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456141662473" ID="ID_1857773936" MODIFIED="1456141756697" POSITION="left" TEXT="les permite">
+<icon BUILTIN="idea"/>
+<node CREATED="1456141667268" ID="ID_413627998" MODIFIED="1456141963492">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Buscar alimento
+    </p>
+    <p>
+      - Defenderse
+    </p>
+    <p>
+      - Relacionarse
+    </p>
+    <p>
+      - Refugiarse
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
