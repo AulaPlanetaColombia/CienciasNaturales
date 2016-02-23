2858aded914cf81db0d40dcd38e1613fe5129612
diegomol88 <diegomol16@gmail.com>
2016-02-23 10:16:17 -0500

    mapa CN 07 03
    mapa CN 07 03

---
+++ fuentes/contenidos/grado07/guion03/CN_07_03_REC240.mm
@@ -0,0 +1,410 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cccccc" CREATED="1456230275689" ID="ID_669279459" MODIFIED="1456233393465" TEXT="La circulaci&#xf3;n en los seres vivos">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1456230486394" ID="ID_711809679" MODIFIED="1456230524253" POSITION="right" TEXT="les permite">
+<icon BUILTIN="idea"/>
+<node CREATED="1456230494160" ID="ID_1903838957" MODIFIED="1456230502992" TEXT="Transportar y distribuir sustancias">
+<node CREATED="1456230504611" ID="ID_492860681" MODIFIED="1456230572993" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1456230514478" ID="ID_1955968490" MODIFIED="1456230839318">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - agua
+    </p>
+    <p>
+      - nutrientes
+    </p>
+    <p>
+      - gases (O<font size="1">2</font>&#160;y CO<font size="1">2</font>)
+    </p>
+    <p>
+      - sustancias de desecho
+    </p>
+    <p>
+      - hormonas
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1456230863252" ID="ID_775998998" MODIFIED="1456230878274" TEXT="por todo">
+<icon BUILTIN="idea"/>
+<node CREATED="1456230871843" ID="ID_737014372" MODIFIED="1456230875465" TEXT="el organismo"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456230996235" ID="ID_1160259694" MODIFIED="1456239964979" POSITION="right" TEXT="se realiza ">
+<icon BUILTIN="idea"/>
+<node CREATED="1456231035544" ID="ID_1019185590" MODIFIED="1456232744179" TEXT="A nivel celular">
+<node CREATED="1456232745839" ID="ID_943078477" MODIFIED="1456233419375" TEXT="por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1456232750061" ID="ID_182379215" MODIFIED="1456232755061" TEXT="diversos mecanismos">
+<node CREATED="1456232757837" ID="ID_1681744091" MODIFIED="1456233421893" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1456232760516" ID="ID_1426038480" MODIFIED="1456239062662">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      transporte pasivo
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1456232934078" ID="ID_235012688" MODIFIED="1456240027183" TEXT="el cual">
+<icon BUILTIN="idea"/>
+<node CREATED="1456232941275" ID="ID_42306705" MODIFIED="1456240032793" TEXT="tiene gasto de energ&#xed;a"/>
+</node>
+</node>
+<node CREATED="1456232770316" ID="ID_897551041" MODIFIED="1456239056340">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      transporte activo
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1456232951823" ID="ID_1583854942" MODIFIED="1456240025057" TEXT="el cual ">
+<icon BUILTIN="idea"/>
+<node CREATED="1456232956498" ID="ID_297295048" MODIFIED="1456240039031" TEXT="no tiene gasto de energ&#xed;a"/>
+</node>
+</node>
+<node CREATED="1456232853565" ID="ID_1574568010" MODIFIED="1456232975592">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      transporte masivo o de mol&#233;culas grandes
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1456232982074" ID="ID_1282761512" MODIFIED="1456233429122" TEXT="incluye">
+<icon BUILTIN="idea"/>
+<node CREATED="1456232991721" ID="ID_792206011" MODIFIED="1456240463251" TEXT="endocitosis o ingreso de sustancias">
+<node CREATED="1456233110019" ID="ID_1007689295" MODIFIED="1456233432690" TEXT="por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1456233201219" ID="ID_1642315812" MODIFIED="1456233216825" TEXT="pinocitosis (sustancias l&#xed;quidas)"/>
+<node CREATED="1456233219156" ID="ID_1511513726" MODIFIED="1456233229110" TEXT="fagocitosis (sustancias s&#xf3;lidas)"/>
+</node>
+</node>
+<node CREATED="1456233050223" ID="ID_1798600159" MODIFIED="1456240466406" TEXT="exositosis o excreci&#xf3;n de sustancias"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456233397302" ID="ID_87570486" MODIFIED="1456237517024" POSITION="right" TEXT="presenta">
+<icon BUILTIN="idea"/>
+<node CREATED="1456237490357" ID="ID_64262585" MODIFIED="1456238281292" TEXT="Variaciones en los organismos">
+<node CREATED="1456233525538" ID="ID_1235029368" MODIFIED="1456236520909" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1456233624008" ID="ID_1556082793" MODIFIED="1456236174313" TEXT="los microorganismos">
+<node CREATED="1456236411338" ID="ID_655555049" MODIFIED="1456237765811" TEXT="que lo realizan por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1456236426624" ID="ID_1962790719" MODIFIED="1456237583022">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - ciclosis
+    </p>
+    <p>
+      - motores moleculares
+    </p>
+    <p>
+      - microt&#250;bulos
+    </p>
+    <p>
+      - ves&#237;culas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1456236515606" ID="ID_1593148220" MODIFIED="1456236517895" TEXT="los hongos">
+<node CREATED="1456237590177" ID="ID_1815108946" MODIFIED="1456237769132" TEXT="por medio de ">
+<icon BUILTIN="idea"/>
+<node CREATED="1456237611916" ID="ID_1321208003" MODIFIED="1456237653630" TEXT="osmotrof&#xed;a"/>
+</node>
+</node>
+<node CREATED="1456236520909" ID="ID_1940004654" MODIFIED="1456237975189" TEXT="las plantas">
+<node CREATED="1456237746536" ID="ID_1387646841" MODIFIED="1456237771370" TEXT="que la realizan">
+<icon BUILTIN="idea"/>
+<node CREATED="1456237754904" ID="ID_468083583" MODIFIED="1456237791016">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      en tres etapas:
+    </p>
+    <p>
+      - absorci&#243;n
+    </p>
+    <p>
+      - transporte
+    </p>
+    <p>
+      - distribuci&#243;n
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1456238013728" ID="ID_509796585" MODIFIED="1456238043433" TEXT="que pueden ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1456238024133" ID="ID_1206336394" MODIFIED="1456238034994" TEXT="no vasculares">
+<node CREATED="1456238052552" ID="ID_1647458100" MODIFIED="1456238324889" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1456238057911" ID="ID_752694715" MODIFIED="1456238082401">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - musgos
+    </p>
+    <p>
+      - hep&#225;ticas
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1456238124250" ID="ID_1089046720" MODIFIED="1456238215666" TEXT="y se clasifican seg&#xfa;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1456238130684" ID="ID_153036126" MODIFIED="1456238343149">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      la forma de absorci&#243;n de agua en:
+    </p>
+    <p>
+      - ectoh&#237;dricas
+    </p>
+    <p>
+      - endoh&#237;dricas
+    </p>
+    <p>
+      - mixoh&#237;dricas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456238037928" ID="ID_330061752" MODIFIED="1456238040700" TEXT="vasculares">
+<node CREATED="1456238417119" ID="ID_1565322736" MODIFIED="1456240438823" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1456238421499" ID="ID_503365251" MODIFIED="1456238440078">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - helechos
+    </p>
+    <p>
+      - gimnospermas
+    </p>
+    <p>
+      - angiospermas
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1456238703819" ID="ID_862929427" MODIFIED="1456240442765" TEXT="y presentan ">
+<icon BUILTIN="idea"/>
+<node CREATED="1456238718505" ID="ID_1350265072" MODIFIED="1456238761882">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      procesos como:
+    </p>
+    <p>
+      - capilaridad
+    </p>
+    <p>
+      - evapotranspiraci&#243;n
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456238614670" ID="ID_775594706" MODIFIED="1456240440214" TEXT="que poseen">
+<icon BUILTIN="idea"/>
+<node CREATED="1456238627011" ID="ID_495746629" MODIFIED="1456238790513" TEXT="tejidos especializados">
+<node CREATED="1456238792908" ID="ID_1684221025" MODIFIED="1456240434390" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1456238799758" ID="ID_432133222" MODIFIED="1456238809425" TEXT="el xilema (transporte de savia bruta)"/>
+<node CREATED="1456238811580" ID="ID_358826634" MODIFIED="1456238820934" TEXT="el floema (transporte de savia elaborada)"/>
+</node>
+<node CREATED="1456238946556" ID="ID_367897063" MODIFIED="1456240431797" TEXT="en">
+<icon BUILTIN="idea"/>
+<node CREATED="1456238953466" ID="ID_1061661494" MODIFIED="1456238966467">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - ra&#237;ces
+    </p>
+    <p>
+      - tallo
+    </p>
+    <p>
+      - hojas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456236525587" ID="ID_1370539605" MODIFIED="1456236528342" TEXT="los animales">
+<node CREATED="1456239130293" ID="ID_161964603" MODIFIED="1456239888756" TEXT="que la realizan">
+<icon BUILTIN="idea"/>
+<node CREATED="1456239292552" ID="ID_825457838" MODIFIED="1456239302041" TEXT="entre las c&#xe9;lulas y el medio externo">
+<node CREATED="1456239347662" ID="ID_686538383" MODIFIED="1456239893025" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1456239351984" ID="ID_701627673" MODIFIED="1456239374876">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - las esponjas de mar
+    </p>
+    <p>
+      - los cnidarios
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1456239199602" ID="ID_555904957" MODIFIED="1456239290791" TEXT="con sistema circulatorio ">
+<node CREATED="1456239416312" ID="ID_919499826" MODIFIED="1456239896876" TEXT="que puede ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1456239425906" ID="ID_1315852287" MODIFIED="1456239450221" TEXT="abierto">
+<node CREATED="1456239631571" FOLDED="true" ID="ID_786411672" MODIFIED="1456240428332" TEXT="como en">
+<icon BUILTIN="idea"/>
+<node CREATED="1456239635792" ID="ID_1995348928" MODIFIED="1456239670819">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - artr&#243;podos
+    </p>
+    <p>
+      - muchos moluscos
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1456239452497" ID="ID_1345714107" MODIFIED="1456239455642" TEXT="cerrado">
+<node CREATED="1456239598335" ID="ID_1197224440" MODIFIED="1456239902036" TEXT="y tener">
+<icon BUILTIN="idea"/>
+<node CREATED="1456239601901" ID="ID_88050138" MODIFIED="1456239619680">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - circulaci&#243;n simple
+    </p>
+    <p>
+      - circulaci&#243;n doble
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1456239724858" ID="ID_1765219790" MODIFIED="1456239905081" TEXT="como en">
+<icon BUILTIN="idea"/>
+<node CREATED="1456239728509" ID="ID_1352626632" MODIFIED="1456239796334">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - an&#233;lidos
+    </p>
+    <p>
+      - cefal&#243;podos
+    </p>
+    <p>
+      - Vertebrados&#160;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1456239468596" ID="ID_602176934" MODIFIED="1456239899244" TEXT="se compone">
+<icon BUILTIN="idea"/>
+<node CREATED="1456239472819" ID="ID_903541558" MODIFIED="1456239525591">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - coraz&#243;n
+    </p>
+    <p>
+      - vasos circulatorios
+    </p>
+    <p>
+      - l&#237;quidos circulantes
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
