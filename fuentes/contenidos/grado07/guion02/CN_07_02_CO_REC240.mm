21afd1109e718b150e4fe304ab67737a94f56a1e
diegomol88 <diegomol16@gmail.com>
2016-04-17 00:12:28 -0500

    mapa CN 07 02
    mapa CN 07 02

---
+++ fuentes/contenidos/grado07/guion02/CN_07_02_CO_REC240.mm
@@ -0,0 +1,238 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cccccc" CREATED="1460865971473" ID="ID_714511187" MODIFIED="1460869265732" TEXT="El sistema locomotor en el ser humano">
+<node CREATED="1460867654851" ID="ID_1001605889" MODIFIED="1460867663515" POSITION="right" TEXT="tiene como funci&#xf3;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1460867665577" ID="ID_1710395236" MODIFIED="1460869538938">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Brindar sost&#233;n
+    </p>
+    <p>
+      - Realizar movimientos
+    </p>
+    <p>
+      - Dar forma al cuerpo
+    </p>
+    <p>
+      - Proteger &#243;rganos
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1460867894451" ID="ID_1348190228" MODIFIED="1460867930187" POSITION="right" TEXT="est&#xe1; formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1460867932880" ID="ID_1180172660" MODIFIED="1460867943043" TEXT="El sistema esquel&#xe9;tico">
+<node CREATED="1460868084487" ID="ID_536465494" MODIFIED="1460868125149" TEXT="que se divide en">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868126897" ID="ID_204314838" MODIFIED="1460868154332" TEXT="huesos">
+<node CREATED="1460869078652" ID="ID_1062015548" MODIFIED="1460869114274" TEXT="formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1460869084377" ID="ID_452160078" MODIFIED="1460869091604" TEXT="tejido &#xf3;seo">
+<node CREATED="1460869119308" ID="ID_1721431041" MODIFIED="1460869182443" TEXT="compuesto por">
+<icon BUILTIN="idea"/>
+<node CREATED="1460869129712" ID="ID_1524471056" MODIFIED="1460869590312">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - osteoclastos
+    </p>
+    <p>
+      - osteoblastos
+    </p>
+    <p>
+      - osteocitos
+    </p>
+    <p>
+      - matriz &#243;sea
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1460868137445" ID="ID_1055665803" MODIFIED="1460868150989" TEXT="cart&#xed;lagos">
+<node CREATED="1460869233036" ID="ID_473192317" MODIFIED="1460869245202" TEXT="formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1460869237374" ID="ID_378046284" MODIFIED="1460869241975" TEXT="tejido cartilaginoso"/>
+</node>
+</node>
+<node CREATED="1460868155894" ID="ID_1732386707" MODIFIED="1460868164284" TEXT="articulaciones">
+<node CREATED="1460869171383" ID="ID_793656110" MODIFIED="1460869178426" TEXT="pueden ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1460869200564" ID="ID_1971345247" MODIFIED="1460869221366">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - m&#243;viles
+    </p>
+    <p>
+      - semim&#243;viles
+    </p>
+    <p>
+      - inm&#243;viles
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1460868175493" ID="ID_1966374440" MODIFIED="1460868179428" TEXT="puede ser">
+<node CREATED="1460868181131" ID="ID_805457852" MODIFIED="1460868187605" TEXT="axial">
+<node CREATED="1460868202092" ID="ID_1929583282" MODIFIED="1460868263639" TEXT="formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868210129" ID="ID_897499645" MODIFIED="1460868256410">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - cabeza
+    </p>
+    <p>
+      - columna vertebral
+    </p>
+    <p>
+      - estern&#243;n
+    </p>
+    <p>
+      - costillas
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1460868189484" ID="ID_1434666860" MODIFIED="1460868199133" TEXT="apendicular">
+<node CREATED="1460868266051" ID="ID_99422264" MODIFIED="1460868273855" TEXT="formado por">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868271415" ID="ID_1577564067" MODIFIED="1460868322393">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - extremidades
+    </p>
+    <p>
+      - hombros
+    </p>
+    <p>
+      - cadera
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1460868330280" ID="ID_1337341563" MODIFIED="1460868336886" TEXT="El sistema muscular">
+<node CREATED="1460868454199" ID="ID_1689776399" MODIFIED="1460868512688" TEXT="que permite">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868480535" ID="ID_1178682891" MODIFIED="1460868483762" TEXT="el movimiento">
+<node CREATED="1460868484962" ID="ID_328533478" MODIFIED="1460868510288" TEXT="por medio de">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868489016" ID="ID_904920437" MODIFIED="1460868506548" TEXT="contracci&#xf3;n y relajaci&#xf3;n muscular"/>
+</node>
+</node>
+</node>
+<node CREATED="1460868516078" ID="ID_1841497650" MODIFIED="1460868618999" TEXT="constituido por">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868557335" ID="ID_315233819" MODIFIED="1460868692954" TEXT="m&#xfa;sculos">
+<node CREATED="1460868563130" ID="ID_1633297025" MODIFIED="1460868617032" TEXT="formados por">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868603945" ID="ID_1783187508" MODIFIED="1460868613763" TEXT="fibras musculares de actina y miosina"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1460868635953" ID="ID_528365838" MODIFIED="1460868648800" POSITION="right" TEXT="puede sufrir">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868650528" ID="ID_1590310983" MODIFIED="1460868657688" TEXT="Enfermedades o lesiones">
+<node CREATED="1460868752966" ID="ID_850001455" MODIFIED="1460868772857" TEXT="que afectan a">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868762769" ID="ID_846118548" MODIFIED="1460868765074" TEXT="los huesos">
+<node CREATED="1460868804741" ID="ID_160587974" MODIFIED="1460868813528" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868814988" ID="ID_154975062" MODIFIED="1460868863829">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - artritis
+    </p>
+    <p>
+      - artrosis
+    </p>
+    <p>
+      - osteoporosis
+    </p>
+    <p>
+      - fracturas
+    </p>
+    <p>
+      - dislocaciones
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1460868767500" ID="ID_420234220" MODIFIED="1460868771546" TEXT="los m&#xfa;sculos">
+<node CREATED="1460868866745" ID="ID_1829614848" MODIFIED="1460868874590" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1460868876095" ID="ID_1040539030" MODIFIED="1460868955494">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - atrofias
+    </p>
+    <p>
+      - desgarros
+    </p>
+    <p>
+      - calambres
+    </p>
+    <p>
+      - distrofias musculares
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
