72107e2c48c9a7091e446a982782492c60a8a8f0
pketa <pketa.gar@gmail.com>
2016-04-21 11:10:18 -0500

    Documentación completa cn_09_12_co

---
+++ fuentes/contenidos/grado09/guion12/CN_09_12_CO.mm
@@ -0,0 +1,87 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1434035905086" ID="ID_215860670" MODIFIED="1434035956441" TEXT="Las leyes de Newton">
+<node CREATED="1434035959571" ID="ID_1405901227" MODIFIED="1457714077348" POSITION="right" TEXT="porpuestas por">
+<icon BUILTIN="idea"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1434035970859" HGAP="27" ID="ID_113062881" MODIFIED="1457712934949" TEXT="Isaac Newton" VSHIFT="12"/>
+</node>
+<node CREATED="1434036480158" ID="ID_1229914150" MODIFIED="1435107656572" POSITION="right" TEXT="son" VSHIFT="2">
+<icon BUILTIN="idea"/>
+<node CREATED="1434036524708" ID="ID_140592166" MODIFIED="1457712958688" TEXT="1.Ley de la Inercia" VSHIFT="-25">
+<node CREATED="1434562718956" ID="ID_1938821737" MODIFIED="1457713561910" TEXT="determina que " VSHIFT="2">
+<icon BUILTIN="idea"/>
+<node CREATED="1434562737598" HGAP="21" ID="ID_161206222" MODIFIED="1457713773150" TEXT="todo cuerpo tiende a permanecer en su estado de movimiento o de reposo en el que se encuentra en la naturaleza" VSHIFT="2"/>
+</node>
+</node>
+<node CREATED="1434036554751" ID="ID_991273490" MODIFIED="1457712975265" TEXT="2. Ley fundamental de la fuerza, masa y aceleraci&#xf3;n">
+<node CREATED="1435106971557" ID="ID_535521905" MODIFIED="1457713650188" TEXT="determina que">
+<icon BUILTIN="idea"/>
+<node CREATED="1435106983576" ID="ID_501291837" MODIFIED="1457713741105" TEXT="si un cuerpo se mueve por la acci&#xf3;n de una fuerza y en la misma direcci&#xf3;n de esta, la aceleraci&#xf3;n que se produce es directamente proporcional a la fuerza que se aplica e inversamente proporcional a la masa del cuerpo sobre el cual se aplica">
+<node CREATED="1457714070467" ID="Freemind_Link_850684568" MODIFIED="1457714086451" TEXT="formula">
+<icon BUILTIN="idea"/>
+<node CREATED="1457714091292" ID="Freemind_Link_1069918914" MODIFIED="1457714143502" TEXT="F = m &#x2022;  a"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1434036569060" ID="ID_637892032" MODIFIED="1457712981103" TEXT="3. Ley de la acci&#xf3;n y la reacci&#xf3;n" VSHIFT="7">
+<node CREATED="1435106971557" ID="Freemind_Link_118022224" MODIFIED="1457713650188" TEXT="determina que">
+<icon BUILTIN="idea"/>
+<node CREATED="1435106983576" ID="Freemind_Link_47593901" MODIFIED="1457713863954" TEXT="cuando un cuerpo ejerce una fuerza (acci&#xf3;n) sobre otro, este ejercer&#xe1; sobre el primero otra fuerza (reacci&#xf3;n) de igual intensidad y direcci&#xf3;n, pero de sentido contrario">
+<node CREATED="1435107079663" HGAP="24" ID="ID_1165203725" MODIFIED="1457714044515" TEXT="justifica la existencia de las" VSHIFT="2">
+<icon BUILTIN="idea"/>
+<node CREATED="1435107089382" ID="ID_1665646235" MODIFIED="1435107815207" TEXT="Fuerza Normal" VSHIFT="-4"/>
+<node CREATED="1435107110872" ID="ID_1438926726" MODIFIED="1435107166997" TEXT="Fuerza de rozamiento"/>
+<node CREATED="1435107169247" ID="ID_76717570" MODIFIED="1435107679426" TEXT="Fuerza el&#xe1;stica" VSHIFT="12"/>
+<node CREATED="1435107186159" ID="ID_261915680" MODIFIED="1435107817864" TEXT="Fuerza centr&#xed;fuga" VSHIFT="6"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1434035973990" ID="ID_1544616465" MODIFIED="1457709574325" POSITION="right" TEXT="se aplican en " VSHIFT="23">
+<icon BUILTIN="idea"/>
+<node CREATED="1457666637546" ID="_" MODIFIED="1457666647218" TEXT="maquinas simples">
+<node CREATED="1434036585377" ID="ID_1163594856" MODIFIED="1457666657526" TEXT="como" VSHIFT="1">
+<icon BUILTIN="idea"/>
+<node CREATED="1434036611084" ID="ID_1627069671" MODIFIED="1434038872601" TEXT="Palanca">
+<node CREATED="1435107239688" ID="ID_1269448084" MODIFIED="1435107497812" TEXT="tipos">
+<icon BUILTIN="idea"/>
+<node CREATED="1435107246485" ID="ID_223163354" MODIFIED="1435107704594" TEXT="Primer g&#xe9;nero" VSHIFT="-5"/>
+<node CREATED="1435107263011" ID="ID_1328495959" MODIFIED="1435107267978" TEXT="Segundo g&#xe9;nero"/>
+<node CREATED="1435107270870" ID="ID_597487713" MODIFIED="1435107708782" TEXT="Tercer g&#xe9;nero" VSHIFT="5"/>
+</node>
+</node>
+<node CREATED="1434036618424" ID="ID_245546569" MODIFIED="1435107228633" TEXT="Poleas ">
+<node CREATED="1435107285406" ID="ID_1809727566" MODIFIED="1435107505342" TEXT="Tipos">
+<icon BUILTIN="idea"/>
+<node CREATED="1435107292042" ID="ID_1932068304" MODIFIED="1435107297767" TEXT="Simples">
+<node CREATED="1435107301916" FOLDED="true" ID="ID_1723210264" MODIFIED="1457666693557" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1435107317384" ID="ID_1391553185" MODIFIED="1435107320353" TEXT="fija"/>
+<node CREATED="1435107322464" ID="ID_509759373" MODIFIED="1435107329136" TEXT="M&#xf3;vil"/>
+</node>
+</node>
+<node CREATED="1435107333681" ID="ID_1632949421" MODIFIED="1435107346479" TEXT="Polipastos">
+<node CREATED="1435107349451" ID="ID_1246917064" MODIFIED="1457666709249" TEXT="que son">
+<icon BUILTIN="idea"/>
+<node CREATED="1435107354529" ID="ID_521813859" MODIFIED="1435107367258" TEXT="Factorial"/>
+<node CREATED="1435107357264" ID="ID_1630942784" MODIFIED="1435107372508" TEXT="Potencial"/>
+<node CREATED="1435107359718" ID="ID_1273012619" MODIFIED="1435107378043" TEXT="Diferencial"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1434036625785" ID="ID_1345994922" MODIFIED="1434038912341" TEXT="Plano inclinado">
+<node CREATED="1435107389043" ID="ID_1590221410" MODIFIED="1457666674938" TEXT="ejemplo">
+<icon BUILTIN="idea"/>
+<node CREATED="1435107399397" ID="ID_959198107" MODIFIED="1435107404787" TEXT="Tornillo sin fin"/>
+</node>
+</node>
+<node CREATED="1434036633398" HGAP="23" ID="ID_466761162" MODIFIED="1435107794382" TEXT="Torno" VSHIFT="17"/>
+</node>
+</node>
+</node>
+</node>
+</map>
