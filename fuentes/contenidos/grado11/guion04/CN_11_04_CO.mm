3cceac38e58523f81489f37c591945ce44204680
pketa <pketa.gar@gmail.com>
2016-06-23 12:02:34 -0500

    ACTUALIZACIÓN 11_04

---
+++ fuentes/contenidos/grado11/guion04/CN_11_04_CO.mm
@@ -0,0 +1,131 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1462485828204" ID="ID_1307332392" MODIFIED="1466553167220" TEXT="Fen&#xf3;menos ondulatorios">
+<node CREATED="1462488895076" HGAP="50" ID="ID_47313840" MODIFIED="1466558251268" POSITION="left" TEXT="Definici&#xf3;n" VSHIFT="-9">
+<node CREATED="1462488896781" ID="ID_104861739" MODIFIED="1466519043284" TEXT="Son los cambios en la direcci&#xf3;n y velocidad de las ondas, dependiendo del obst&#xe1;culo con que interactuan ">
+<node CREATED="1462490718613" HGAP="49" ID="ID_977542505" MODIFIED="1466558164504" TEXT="se presentan en" VSHIFT="11">
+<node CREATED="1462489017298" ID="ID_302403958" MODIFIED="1466470680393">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - ondas mec&#225;nicas
+    </p>
+    <p>
+      - ondas electromagn&#233;ticas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1466553172500" HGAP="46" ID="ID_157697273" MODIFIED="1466558248398" POSITION="right" TEXT="se clasifican en" VSHIFT="1">
+<node CREATED="1466553246190" HGAP="63" ID="ID_1601693232" MODIFIED="1466558266874" TEXT="reflexi&#xf3;n" VSHIFT="-40">
+<node CREATED="1466553500792" ID="ID_1621868535" MODIFIED="1466571624292" TEXT="se define como el ">
+<icon BUILTIN="idea"/>
+<node CREATED="1466553445805" ID="ID_1742244649" MODIFIED="1466570809551" TEXT="cambio de direcci&#xf3;n en el mismo medio donde la onda se propaga ">
+<node CREATED="1466557568814" HGAP="22" ID="ID_400878762" MODIFIED="1466558240361" TEXT="ocurre cuando" VSHIFT="-20">
+<node CREATED="1466557577959" ID="ID_498265684" MODIFIED="1466570908679" TEXT="una onda llega a la superficie de un medio con caracter&#xed;sticas distintas al de la propagaci&#xf3;n de la onda">
+<node CREATED="1466553616347" ID="ID_825079760" MODIFIED="1466556934397" TEXT="se observa en">
+<node CREATED="1466553620830" ID="ID_1005658249" MODIFIED="1466553659764" TEXT="el reflejo de la luz en un espejo"/>
+</node>
+</node>
+</node>
+<node CREATED="1466557910083" HGAP="22" ID="ID_1578946888" MODIFIED="1466558239011" TEXT="puede ser" VSHIFT="35">
+<node CREATED="1466557913953" ID="ID_109867159" MODIFIED="1466570927215" TEXT="especular">
+<node CREATED="1466570938980" ID="ID_799305270" MODIFIED="1466570952201" TEXT=" todas las ondas se reflejan con el mismo &#xe1;ngulo"/>
+</node>
+<node CREATED="1466557932963" ID="ID_1621192816" MODIFIED="1466570964139" TEXT="difusa">
+<arrowlink DESTINATION="ID_1621192816" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_221296176" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1621192816" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_221296176" SOURCE="ID_1621192816" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1466570982636" ID="ID_367766445" MODIFIED="1466571042253" TEXT=" ondas reflejadas con diferentes &#xe1;ngulos, debido a las irregularidades en la superficie reflectante"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1466553252789" HGAP="38" ID="ID_414212796" MODIFIED="1466558269991" TEXT="refracci&#xf3;n" VSHIFT="-49">
+<node CREATED="1466553256806" ID="ID_1373585238" MODIFIED="1466571626504" TEXT="se define como el">
+<icon BUILTIN="idea"/>
+<node CREATED="1466553521965" ID="ID_839237002" MODIFIED="1466571190578" TEXT="cambio de direcci&#xf3;n y velocidad de la onda al ingresar a un medio de caracter&#xed;sticas diferentes al de propagaci&#xf3;n">
+<node CREATED="1466558165294" HGAP="30" ID="ID_695845734" MODIFIED="1466571540959" TEXT="incluye" VSHIFT="-9">
+<icon BUILTIN="idea"/>
+<node CREATED="1466558165447" ID="ID_1770988329" MODIFIED="1466571262964" TEXT="dispersi&#xf3;n de la luz">
+<node CREATED="1466571265110" ID="ID_1355951634" MODIFIED="1466571270629" TEXT=" la luz blanca se descompone en los colores que la conforman">
+<node CREATED="1466571490215" ID="ID_566015173" MODIFIED="1466571529489" TEXT="se observa en el ">
+<icon BUILTIN="idea"/>
+<node CREATED="1466571524751" ID="ID_1794015679" MODIFIED="1466571527909" TEXT="arco&#xed;ris"/>
+</node>
+</node>
+</node>
+<node CREATED="1466558165942" ID="ID_245940157" MODIFIED="1466571280766" TEXT="polarizaci&#xf3;n">
+<node CREATED="1466571282627" ID="ID_1256371298" MODIFIED="1466571480414" TEXT="permite la vibraci&#xf3;n de las ondas transversales en uno o dos planos">
+<node CREATED="1466571490215" ID="ID_948694344" MODIFIED="1466571579178" TEXT="se observa en los">
+<icon BUILTIN="idea"/>
+<node CREATED="1466571524751" ID="ID_41375830" MODIFIED="1466571588562" TEXT="lentes polaroid"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1466553663542" ID="ID_1510418354" MODIFIED="1466571536304" TEXT="se observa en">
+<arrowlink DESTINATION="ID_1510418354" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1129205295" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1510418354" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1129205295" SOURCE="ID_1510418354" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1466553667931" ID="ID_304860839" MODIFIED="1466553711168" TEXT="la desviaci&#xf3;n de la luz al entrar al agua, que hace que los objetos parcialmente sumergidos se vean torcidos">
+<node CREATED="1466571672744" ID="ID_943729366" MODIFIED="1466571686612" TEXT="se estudia con la ">
+<icon BUILTIN="idea"/>
+<node CREATED="1466571692111" ID="ID_855756920" MODIFIED="1466573906615">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ley de Snell<img src="../../../PLANETA/CN_11_04_CO/LEYSNELL.jpg" />
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1466553260120" HGAP="39" ID="ID_7873800" MODIFIED="1466558272141" TEXT="difracci&#xf3;n" VSHIFT="-12">
+<node CREATED="1466553555080" ID="ID_1238114508" MODIFIED="1466571632520" TEXT="se define como el">
+<icon BUILTIN="idea"/>
+<node CREATED="1466553556116" ID="ID_282947685" MODIFIED="1466553569087" TEXT="cambio de direcci&#xf3;n de la onda para rodear un obst&#xe1;culo">
+<node CREATED="1466557669024" ID="ID_1371756114" MODIFIED="1466571646623" TEXT="ocurre cuando">
+<arrowlink DESTINATION="ID_1371756114" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1297828940" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1371756114" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1297828940" SOURCE="ID_1371756114" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1466557630923" ID="ID_20887317" MODIFIED="1466557655954" TEXT="las ondas llegan a un obst&#xe1;culo que interrumpe parcialmente su propagaci&#xf3;n">
+<node CREATED="1466553718573" ID="ID_239923864" MODIFIED="1466571656910" TEXT="se observa en">
+<icon BUILTIN="idea"/>
+<node CREATED="1466553722970" ID="ID_117846827" MODIFIED="1466557067627" TEXT="los bordes pocos definidos de las sombras"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1466553266362" HGAP="50" ID="ID_479990012" MODIFIED="1466558263329" TEXT="interferencia" VSHIFT="43">
+<node CREATED="1466553570497" ID="ID_343394788" MODIFIED="1466573943774" TEXT="se define como la">
+<icon BUILTIN="idea"/>
+<node CREATED="1466553572176" ID="ID_714387911" MODIFIED="1466574145440" TEXT="suma de dos ondas que se encuentran y que son emitidas por distintas fuentes">
+<node CREATED="1466557853920" ID="ID_206570848" MODIFIED="1466574250479" TEXT="puede ser" VSHIFT="43">
+<icon BUILTIN="idea"/>
+<node CREATED="1466557857400" ID="ID_758500003" MODIFIED="1466557869650" TEXT="constructiva: aumenta la amplitud de las ondas"/>
+<node CREATED="1466557872142" ID="ID_1866259254" MODIFIED="1466557896275" TEXT="destructiva: disminuye la amplitud de las ondas"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
