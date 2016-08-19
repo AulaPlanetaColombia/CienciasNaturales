20cc7a5b1f521d309df51cf7da41fa50b7d8f004
pketa <pketa.gar@gmail.com>
2016-08-19 08:53:14 -0500

    maoa, soliciutes y guion 11_06

---
+++ fuentes/contenidos/grado11/guion06/MapaCN_11_06_CO.mm
@@ -0,0 +1,137 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1471607318230" ID="ID_1395543453" MODIFIED="1471607384455" TEXT="Los espejos y las lentes">
+<node CREATED="1471607345847" ID="ID_876281946" MODIFIED="1471612637973" POSITION="right" TEXT="Espejos">
+<node CREATED="1471608024404" ID="ID_1777853469" MODIFIED="1471612637951" TEXT="son">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612622212" ID="ID_32986351" MODIFIED="1471612654788" TEXT="superficies pulimentadas">
+<node CREATED="1471612655891" ID="ID_865965632" MODIFIED="1471612672065" TEXT="que tienen la propiedad de ">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612674358" ID="ID_1074068794" MODIFIED="1471612684988" TEXT="reflejar la luz"/>
+</node>
+</node>
+</node>
+<node CREATED="1471612706288" ID="ID_1359564374" MODIFIED="1471612754861" TEXT="se clasifican en">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612737345" ID="ID_518731726" MODIFIED="1471612881805" TEXT="planos">
+<node CREATED="1471614243040" ID="ID_1340930379" MODIFIED="1471614275462" TEXT="que tiene">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612755894" ID="ID_1361627190" MODIFIED="1471614300137" TEXT="una superficie plana">
+<node CREATED="1471612779478" ID="ID_1726528687" MODIFIED="1471614306640" TEXT="muestra una imagen">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612785976" ID="ID_1898162823" MODIFIED="1471612847960" TEXT="-virtual -del mismo tama&#xf1;o -derecha"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1471614312813" ID="ID_437429887" MODIFIED="1471614345385" TEXT="esf&#xe9;ricos">
+<node CREATED="1471614243040" ID="ID_864744081" MODIFIED="1471614275462" TEXT="que tiene">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612755894" ID="ID_1376267217" MODIFIED="1471614405354" TEXT="una superficie curva, la cual pertenece a un casquete esf&#xe9;rico">
+<node CREATED="1471612779478" ID="ID_171287241" MODIFIED="1471614451713" TEXT="se clasifican en ">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612785976" ID="ID_149042834" MODIFIED="1471614448252" TEXT="c&#xf3;ncavos">
+<node CREATED="1471614475053" ID="ID_1906098798" MODIFIED="1471614516460" TEXT="su foco y centro de curvatura son reales"/>
+</node>
+<node CREATED="1471614451714" ID="ID_846751776" MODIFIED="1471614455396" TEXT="convexos">
+<node CREATED="1471614518005" ID="ID_138892527" MODIFIED="1471614527004" TEXT="su foco y centro de curvatura son virtuales"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1471607357282" ID="ID_1600634616" MODIFIED="1471612446537" POSITION="left" TEXT="Lentes">
+<node CREATED="1471608040150" ID="ID_1872613216" MODIFIED="1471608048266" TEXT="son">
+<icon BUILTIN="idea"/>
+<node CREATED="1471608050190" ID="ID_1907129507" MODIFIED="1471609560021" TEXT="piezas de material transparente, como el vidrio o pl&#xe1;stico, que cuenta con dos superficies">
+<node CREATED="1471608075055" ID="ID_1752556130" MODIFIED="1471608109959" TEXT="tiene propiedades de ">
+<icon BUILTIN="idea"/>
+<node CREATED="1471608111439" ID="ID_1004040660" MODIFIED="1471609029699" TEXT="refractar la luz">
+<node CREATED="1471609242801" ID="ID_1014147512" MODIFIED="1471609254557" TEXT="se explica por medio de la">
+<icon BUILTIN="idea"/>
+<node CREATED="1471609257609" ID="ID_927568460" MODIFIED="1471612483172" TEXT="ley de Snell"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1471609285429" ID="ID_724908710" MODIFIED="1471612438270" TEXT="se clasifican en ">
+<icon BUILTIN="idea"/>
+<node CREATED="1471609313053" ID="ID_1455230166" MODIFIED="1471611007088" TEXT="convergentes">
+<node CREATED="1471609872960" ID="ID_673731049" MODIFIED="1471610498818" TEXT="se caracterizan por">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1471610500100" ID="ID_578009644" MODIFIED="1471611258402" TEXT="-refractar los rayo de luz y focalizarlos en un punto - son m&#xe1;s gruesas en su centro que en los extremos"/>
+</node>
+<node CREATED="1471611027355" ID="ID_499164723" MODIFIED="1471611048247" TEXT="pueden ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1471611043090" ID="ID_1790131564" MODIFIED="1471611367876" TEXT="-biconvexas -planoconvexas - concavo-conve "/>
+</node>
+</node>
+<node CREATED="1471609797971" ID="ID_970553050" MODIFIED="1471609803870" TEXT="divergentes">
+<node CREATED="1471609872960" ID="ID_735429757" MODIFIED="1471610498818" TEXT="se caracterizan por">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1471610500100" ID="ID_89410920" MODIFIED="1471611410440" TEXT="-refractar los rayo de luz y los separa - son m&#xe1;s delgadas en su centro que en los extremos"/>
+</node>
+<node CREATED="1471611027355" ID="ID_1374629113" MODIFIED="1471611048247" TEXT="pueden ser">
+<icon BUILTIN="idea"/>
+<node CREATED="1471611043090" ID="ID_264219272" MODIFIED="1471611565602" TEXT="-bic&#xf3;ncavas -planoc&#xf3;ncavas - convexo-c&#xf3;ncavas "/>
+</node>
+</node>
+</node>
+<node CREATED="1471612448408" ID="ID_346566100" MODIFIED="1471612457957" TEXT="se utilizan en la ">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612459633" ID="ID_84249883" MODIFIED="1471612507265" TEXT="correcci&#xf3;n de enfermedades de la visi&#xf3;n">
+<node CREATED="1471612510445" ID="ID_1112318399" MODIFIED="1471612515507" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612524304" ID="ID_1736827176" MODIFIED="1471612562581" TEXT="-miop&#xed;a -hipermetrop&#xed;a -astigmatismo -presbicia"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1471607389657" ID="ID_598091758" MODIFIED="1471607461364" POSITION="right" TEXT="se estudian por medio">
+<icon BUILTIN="idea"/>
+<node CREATED="1471607418600" ID="ID_856172041" MODIFIED="1471607450629" TEXT="&#xd3;ptica">
+<node CREATED="1471607482540" ID="ID_1550116839" MODIFIED="1471607553093" TEXT="se dividen en">
+<icon BUILTIN="idea"/>
+<node CREATED="1471607506573" ID="ID_1163979614" MODIFIED="1471607525801" TEXT="&#xf3;ptica f&#xed;sica">
+<node CREATED="1471607547746" ID="ID_912379053" MODIFIED="1471607751974" TEXT="estudia">
+<icon BUILTIN="idea"/>
+<node CREATED="1471607597450" ID="ID_901960320" MODIFIED="1471607948172" TEXT="interacci&#xf3;n de la luz con obst&#xe1;culos de tama&#xf1;o del orden de la longitud de onda de la luz">
+<node CREATED="1471607759357" ID="ID_159553376" MODIFIED="1471607888222" TEXT="estudia fen&#xf3;menos como ">
+<icon BUILTIN="idea"/>
+<node CREATED="1471607770423" ID="ID_305337623" MODIFIED="1471607937499" TEXT="-interferencia -difracci&#xf3;n -polarizaci&#xf3;n"/>
+</node>
+</node>
+<node CREATED="1471610467934" ID="ID_292983316" MODIFIED="1471610489233" TEXT="la luz como una onda"/>
+</node>
+</node>
+<node CREATED="1471607527618" ID="ID_126992110" MODIFIED="1471607538865" TEXT="&#xf3;ptica geom&#xe9;trica">
+<node CREATED="1471607578178" ID="ID_477150910" MODIFIED="1471610427563" TEXT="estudia">
+<icon BUILTIN="idea"/>
+<node CREATED="1471607682461" ID="ID_1118482411" MODIFIED="1471607714799" TEXT="interacci&#xf3;n con obst&#xe1;culos u objetos mayores a la longitud de onda de la luz">
+<node CREATED="1471607759357" ID="ID_1329486820" MODIFIED="1471607888222" TEXT="estudia fen&#xf3;menos como ">
+<icon BUILTIN="idea"/>
+<node CREATED="1471607770423" ID="ID_1167906173" MODIFIED="1471607987679" TEXT="-reflexi&#xf3;n - refracci&#xf3;n"/>
+</node>
+</node>
+<node CREATED="1471610427567" ID="ID_931669900" MODIFIED="1471610453213" TEXT="la luz como un haz de rayos luminosos"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1471611604777" ID="ID_1232900673" MODIFIED="1471612306583" POSITION="left" TEXT="se utilizan en ">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612008090" ID="ID_1383743676" MODIFIED="1471612022581" TEXT="Instrumentos &#xf3;pticos">
+<node CREATED="1471612311041" ID="ID_342840501" MODIFIED="1471612316357" TEXT="como">
+<icon BUILTIN="idea"/>
+<node CREATED="1471612319030" ID="ID_498708437" MODIFIED="1471612607255" TEXT="-lupa -microscopio -telesc&#xf3;pio -la c&#xe1;mara fotogr&#xe1;fica"/>
+</node>
+</node>
+</node>
+</node>
+</map>
